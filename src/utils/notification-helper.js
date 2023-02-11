9484658ff893c4b979473e618979764b6a67aabf
Ka Hung Lee <lee.kahung414@gmail.com>
2023-02-10 17:49:31 -0800

    Revert "Removing notification-helper since it's been replaced with Redux reducers"
    This reverts commit 0f782881329b218e87e03df113624cfb96fb9ae3.

---
+++ src/utils/notification-helper.js
@@ -0,0 +1,25 @@
+export const runNotification = (
+  message,
+  time, // in seconds
+  timeoutID,
+  setStatusFunc,
+  setTimeoutFunc
+) => {
+  // if local timeoutID exist, clear previous message and
+  // run new status notification message
+  if (timeoutID) {
+    clearTimeout(timeoutID);
+    setStatusFunc({ state: true, message });
+  }
+
+  // set timeout for potentially new notification
+  const timeout = setTimeout(() => {
+    setStatusFunc({
+      state: false,
+      message: "",
+    });
+  }, time * 1000);
+
+  setTimeoutFunc(timeout);
+  setStatusFunc({ state: true, message });
+};
