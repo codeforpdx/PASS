3c7f0002f59df56d6aed48734e23d65cc7486504
Greg Witt <greg.witt@avnet.com>
2023-03-01 19:20:51 -0500

    adds eslint and prettier dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb",
+        "prettier"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 14
+    },
+    "plugins": [
+        "prettier"
+    ],
+    "rules": {
+    }
+};
