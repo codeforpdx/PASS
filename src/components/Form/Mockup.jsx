b6ec72c7d773b39247066467a870536200332d38
Andy <awilliamscomposing@gmail.com>
2023-04-02 21:52:00 -0700

    Implementing data grid

--- src/components/Form/Mockup.jsx
+++ src/components/Form/Mockup.jsx
@@ -1,37 +1,100 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
+// import Checkbox from '@mui/material/Checkbox';
+// import Paper from '@mui/material/Paper';
+// import Stack from '@mui/material/Stack';
+// import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+// import SendIcon from '@mui/icons-material/Send';
+import { DataGrid } from '@mui/x-data-grid';
+import Modal from '@mui/material/Modal';
 import NavBar from './NavBar';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary
-}));
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90
+  },
+  { field: 'priority', headerName: 'Priority', type: 'checkbox', width: 75 }
+];
 
-const Mockup = () => (
-  <>
-    <NavBar />
-    <br />
-    <Box sx={{ width: '100%' }}>
-      <Stack spacing={2}>
-        <Typography>
-          <div>Clients Date Modified Priority</div>
-        </Typography>
-        <Item>Rachel Jan 1, 2022, Checkmark</Item>
-        <Item>Rachel Jan 1, 2022, Checkmark</Item>
-        <Item>Rachel Jan 1, 2022, Checkmark</Item>
-        <Item>Rachel Jan 1, 2022, Checkmark</Item>
-        <Item>Rachel Jan 1, 2022, Checkmark</Item>
-        <Item>Rachel Jan 1, 2022, Checkmark</Item>
-      </Stack>
-    </Box>
-  </>
-);
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35, priority: 'Y' },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 }
+];
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4
+};
+
+const Mockup = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <NavBar />
+      <br />
+      <Button variant="outlined" startIcon={<DeleteIcon />} disabled>
+        Delete
+      </Button>
+      <div>
+        <Button onClick={handleOpen}>Open modal</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+          {/* <Button variant="outlined" startIcon={<DeleteIcon />} disabled>
+            Delete
+          </Button> */}
+        </Modal>
+      </div>
+      <Box sx={{ width: '100%' }}>
+        <div style={{ height: 400, width: '100%' }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+          />
+        </div>
+      </Box>
+    </>
+  );
+};
 
 export default Mockup;
