6464a3532afa284032b66810afb064a3187f7f5f
Emily Sellers <emilymelindasellers@gmail.com>
2023-04-11 11:41:33 -0700

    add confirmation dialogue for clicking logout button

--- src/components/Login/Logout.jsx
+++ src/components/Login/Logout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSession, LogoutButton } from '@inrupt/solid-ui-react';
 
 /**
@@ -11,6 +11,7 @@ import { useSession, LogoutButton } from '@inrupt/solid-ui-react';
 
 const Logout = () => {
   const { session } = useSession();
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
   return (
     <section id="logout" className="panel">
@@ -18,9 +19,20 @@ const Logout = () => {
         <label id="labelLogout" htmlFor="btnLogout">
           Click the following logout button to log out of your pod:{' '}
         </label>
-        <LogoutButton>
-          <button type="submit">Logout</button>
-        </LogoutButton>
+        <button type="submit" onClick={() => setShowConfirmation(true)}>
+          Logout
+        </button>
+        {showConfirmation ? (
+          <div>
+            <p>Are you sure you want to log out?</p>
+            <LogoutButton>
+              <button>Yes</button>
+            </LogoutButton>
+            <button onClick={() => setShowConfirmation(false)}>No</button>
+          </div>
+        ) : (
+          <p></p>
+        )}
         <p className="labelStatus" role="alert">
           Your session is now logged in with the WebID [
           <a href={session.info.webId} target="_blank" rel="noreferrer">
@@ -34,3 +46,48 @@ const Logout = () => {
 };
 
 export default Logout;
+
+// const Logout = () => {
+//   const { session } = useSession();
+//   const [showConfirmation, setShowConfirmation] = useState(false);
+
+//   const handleLogout = () => {
+//     setShowConfirmation(true);
+//   };
+
+//   const confirmLogout = () => {
+//     LogoutButton.handleLogout();
+//   };
+
+//   return (
+//     <section id="logout" className="panel">
+//       <div className="row">
+//         <label id="labelLogout" htmlFor="btnLogout">
+//           Click the following logout button to log out of your pod:{' '}
+//         </label>
+//         {showConfirmation ? (
+//           <div>
+//             <p>Are you sure you want to log out?</p>
+//             <button onClick={confirmLogout}>Yes</button>
+//             <button onClick={() => setShowConfirmation(false)}>No</button>
+//           </div>
+//         ) : (
+//           <LogoutButton>
+//             <button type="submit" onClick={handleLogout}>
+//               Logout
+//             </button>
+//           </LogoutButton>
+//         )}
+//         <p className="labelStatus" role="alert">
+//           Your session is now logged in with the WebID [
+//           <a href={session.info.webId} target="_blank" rel="noreferrer">
+//             {session.info.webId}
+//           </a>
+//           ].
+//         </p>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Logout;
