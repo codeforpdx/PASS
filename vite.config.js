09b1f6f0cb3a75b9452781ca6047ad07def3528b
Ka Hung Lee <lee.kahung414@gmail.com>
2023-04-26 06:07:55 -0700

    Updating vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import visualizer from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-    base: '/PASS/',
-    plugins: [react()],
-})
+  base: '/PASS/',
+  plugins: [
+    react(),
+    visualizer({
+      filename: 'bundleStats.html',
+      open: true
+    })
+  ]
+});
