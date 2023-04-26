b3d48d02e2009271773ce98770f53f6800b5fac4
Ka Hung Lee <lee.kahung414@gmail.com>
2023-04-26 05:52:19 -0700

    Updating Login and removing prop for redirectUrl

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ const App = () => {
                   }
                 />
               ) : (
-                <Login redirectUrl={redirectUrl} />
+                <Login />
               )
             }
           />
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { LoginButton } from '@inrupt/solid-ui-react';
+import { useRedirectUrl } from '../../hooks';
 import { SOLID_IDENTITY_PROVIDER } from '../../utils';
 import AppHeader from '../AppHeader';
 
@@ -11,27 +12,31 @@ import AppHeader from '../AppHeader';
  * @name Login
  */
 
-const Login = ({ redirectUrl }) => (
-  <>
-    <AppHeader />
-    <section id="login" className="panel">
-      <div className="row">
-        <label id="labelLogin" htmlFor="btnLogin">
-          Click the following login button to log into your pod at [
-          <a href={SOLID_IDENTITY_PROVIDER} target="_blank" rel="noreferrer">
-            {SOLID_IDENTITY_PROVIDER}
-          </a>
-          ]:{' '}
-        </label>
-        <LoginButton
-          oidcIssuer={SOLID_IDENTITY_PROVIDER}
-          redirectUrl={redirectUrl}
-          onError={console.error}
-        />
-        <p>Not logged in</p>
-      </div>
-    </section>
-  </>
-);
+const Login = () => {
+  const redirectUrl = useRedirectUrl();
+
+  return (
+    <>
+      <AppHeader />
+      <section id="login" className="panel">
+        <div className="row">
+          <label id="labelLogin" htmlFor="btnLogin">
+            Click the following login button to log into your pod at [
+            <a href={SOLID_IDENTITY_PROVIDER} target="_blank" rel="noreferrer">
+              {SOLID_IDENTITY_PROVIDER}
+            </a>
+            ]:{' '}
+          </label>
+          <LoginButton
+            oidcIssuer={SOLID_IDENTITY_PROVIDER}
+            redirectUrl={redirectUrl}
+            onError={console.error}
+          />
+          <p>Not logged in</p>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Login;
