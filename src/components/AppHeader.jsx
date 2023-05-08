a8dec0c4a42db2e911302b425eae1aec45e75f63
Andy <awilliamscomposing@gmail.com>
2023-05-07 17:53:01 -0700

    Styling only existing elements

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSession } from '@inrupt/solid-ui-react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 /**
  * AppHeader Component - Component that generates AppHeader section for PASS
@@ -12,24 +19,40 @@ import { useSession } from '@inrupt/solid-ui-react';
 const AppHeader = () => {
   const { session } = useSession();
 
-  const headerStyle = {
-    width: '100%',
-    gap: '20px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContents: 'center'
-  };
-
   return (
-    <header style={headerStyle}>
-      <h2>Getting Started with PASS</h2>
-      {session.info.isLoggedIn ? (
-        <>
-          <Link to="/PASS/home/">Home</Link>
-          <Link to="/PASS/forms/">Forms</Link>
-        </>
-      ) : null}
-    </header>
+    <>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            {session.info.isLoggedIn ? (
+              <>
+                <Typography variant="h4" noWrap component="div">
+                  PASS
+                </Typography>
+                <ButtonGroup
+                  variant="contained"
+                  aria-label="outlined primary button group"
+                  color="tertiary"
+                  sx={{ margin: '0 0 0 20px' }}
+                >
+                  <Link to="/PASS/home/">
+                    <Button>Home</Button>
+                  </Link>
+                  <Link to="/PASS/forms/">
+                    <Button>Forms</Button>
+                  </Link>
+                </ButtonGroup>
+              </>
+            ) : (
+              <Typography variant="h4" noWrap component="div">
+                Getting started with PASS
+              </Typography>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
   );
 };
 
