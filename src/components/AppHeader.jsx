99370f92ce4ac2317a784d6dd0f41f6b07cfc427
Andy <awilliamscomposing@gmail.com>
2023-05-07 18:13:44 -0700

    Adding icons from in-progress design

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSession } from '@inrupt/solid-ui-react';
 import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
+import Badge from '@mui/material/Badge';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
+import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SearchIcon from '@mui/icons-material/Search';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
@@ -20,39 +24,54 @@ const AppHeader = () => {
   const { session } = useSession();
 
   return (
-    <>
-      <CssBaseline />
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" color="primary">
-          <Toolbar>
-            {session.info.isLoggedIn ? (
-              <>
-                <Typography variant="h4" noWrap component="div">
-                  PASS
-                </Typography>
-                <ButtonGroup
-                  variant="contained"
-                  aria-label="outlined primary button group"
-                  color="tertiary"
-                  sx={{ margin: '0 0 0 20px' }}
-                >
-                  <Link to="/PASS/home/">
-                    <Button>Home</Button>
-                  </Link>
-                  <Link to="/PASS/forms/">
-                    <Button>Forms</Button>
-                  </Link>
-                </ButtonGroup>
-              </>
-            ) : (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          {session.info.isLoggedIn ? (
+            <>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
               <Typography variant="h4" noWrap component="div">
-                Getting started with PASS
+                PASS
               </Typography>
-            )}
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </>
+              <ButtonGroup
+                variant="contained"
+                aria-label="outlined primary button group"
+                color="tertiary"
+                sx={{ margin: '0 0 0 20px' }}
+              >
+                <Link to="/PASS/home/">
+                  <Button>Home</Button>
+                </Link>
+                <Link to="/PASS/forms/">
+                  <Button>Forms</Button>
+                </Link>
+              </ButtonGroup>
+              <Box sx={{ flexGrow: 1 }} />
+              <IconButton size="large" color="inherit">
+                <SearchIcon />
+              </IconButton>
+              <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
+                <Badge badgeContent={17} color="error">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+            </>
+          ) : (
+            <Typography variant="h4" noWrap component="div">
+              Getting started with PASS
+            </Typography>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
