c5752756b7414a010710e09b3af68632d795b0b8
Ben <benawhiting@gmail.com>
2023-05-14 11:36:54 -0700

    Improve failure-on-send error message

--- src/components/Inbox/NewMessage.jsx
+++ src/components/Inbox/NewMessage.jsx
@@ -51,7 +51,7 @@ const NewMessage = () => {
         setSuccess(`Message successfully sent to ${message.recipientUsername}`);
       } catch (err) {
         // TODO: Make sure invalid username is the only possible error
-        setError('Recipient username does not exist');
+        setError('Recipient username not found');
       }
     }
   };
