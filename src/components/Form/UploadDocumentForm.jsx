24103f6a76f2b016aee5c62ab2fc51bedc67fde2
tomit4 <mosssap@gmail.com>
2023-05-17 21:08:31 -0700

    :construction: Subtle changes to accepted image formats and test gif name

--- src/components/Form/UploadDocumentForm.jsx
+++ src/components/Form/UploadDocumentForm.jsx
@@ -145,7 +145,7 @@ const UploadDocumentForm = () => {
             id="upload-doctype"
             type="file"
             name="uploadDoctype"
-            accept=".pdf, .docx, .doc, .txt, .rtf"
+            accept=".pdf, .docx, .doc, .txt, .rtf, .gif"
             onChange={handleFileChange}
           />
           <button disabled={state.processing} type="submit">
