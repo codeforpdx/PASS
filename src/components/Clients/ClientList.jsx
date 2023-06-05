1a9ffa4dd1cfd69189765d32e6840addadd6a720
veganedge <scottxbrown@yahoo.com>
2023-06-04 18:26:28 -0700

    fixing error due to userList -> userListObject now

--- src/components/Clients/ClientList.jsx
+++ src/components/Clients/ClientList.jsx
@@ -9,7 +9,6 @@ import { UserListContext } from '../../contexts';
 // Component Imports
 import ClientListTable from './ClientListTable';
 
-
 /**
  * ClientList Component - Component that generates ClientList section for PASS
  * which interfaces with Solid Pod to fetch user list
@@ -19,33 +18,31 @@ import ClientListTable from './ClientListTable';
  */
 
 const ClientList = ({ loadingActive }) => {
-  const { userList } = useContext(UserListContext);
+  const { userListObject } = useContext(UserListContext);
 
-  return (
-    userList.length ? (
-      // render if clients
-      <ClientListTable
-        loadingActive={loadingActive}
-        statusType="Status"
-        defaultMessage="No actions"
-      />
-    ) : (
-      // render if no clients
-      <Container>
-        <Box
-          sx={{
-            marginTop: 3,
-            minWidth: 120,
-            display: 'flex',
-            flexDirection: 'column'
-          }}
-        >
-          <Typography variant="h6" component="h2" mb={2} align="center" color="secondary">
-            Add clients to your list
-          </Typography>
-        </Box>
-      </Container>
-    )
+  return userListObject.userList ? (
+    // render if clients
+    <ClientListTable
+      loadingActive={loadingActive}
+      statusType="Status"
+      defaultMessage="No actions"
+    />
+  ) : (
+    // render if no clients
+    <Container>
+      <Box
+        sx={{
+          marginTop: 3,
+          minWidth: 120,
+          display: 'flex',
+          flexDirection: 'column'
+        }}
+      >
+        <Typography variant="h6" component="h2" mb={2} align="center" color="secondary">
+          Add clients to your list
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
