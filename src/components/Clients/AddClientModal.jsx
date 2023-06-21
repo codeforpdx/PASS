3fe30a6958f14ef4b4b4e71b0d160f6b270c8852
Andy <awilliamscomposing@gmail.com>
2023-06-21 13:14:28 -0700

    Preparing as a draft for review

--- src/components/Clients/AddClientModal.jsx
+++ src/components/Clients/AddClientModal.jsx
@@ -3,16 +3,14 @@ import React, { useContext, useState } from 'react';
 // Inrupt Library Imports
 import { useSession } from '@inrupt/solid-ui-react';
 // Material UI Imports
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import CheckIcon from '@mui/icons-material/Check';
+import ClearIcon from '@mui/icons-material/Clear';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
-import ClearIcon from '@mui/icons-material/Clear';
-import CheckIcon from '@mui/icons-material/Check';
 import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import TextField from '@mui/material/TextField';
 // Utility Imports
 import { ENV } from '../../constants';
 import { runNotification } from '../../utils';
@@ -25,11 +23,11 @@ import { UserListContext } from '../../contexts';
 import FormSection from '../Form/FormSection';
 
 /**
- * AddClient Component - Component that allows users to add other user's
+ * AddClientModal Component - Component that allows users to add other user's
  * Pod URLs from a user's list stored on their own Pod
  *
- * @memberof Forms
- * @name AddClient
+ * @memberof Clients
+ * @name AddClientModal
  */
 
 const renderWebId = (username) => {
@@ -141,108 +139,87 @@ const AddClientModal = ({ showModal, setShowModal }) => {
         statusType="Status"
         defaultMessage="To be added..."
       >
-        <Box display="flex" justifyContent="center">
-          <form
-            onSubmit={handleAddClient}
-            // style={{ marginTop: '20px', marginBottom: '20px' }}
-            autoComplete="off"
-          >
-            <FormControl fullWidth>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    autoComplete="given-name"
-                    name="addUserGivenName"
-                    required
-                    fullWidth
-                    id="add-user-given-name"
-                    label="First/given name"
-                    autoFocus
-                    {...userGivenName}
-                  />
-                </Grid>
-                <br />
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="add-user-last-name"
-                    label="Last/family name"
-                    name="addUserFamilyName"
-                    autoComplete="family-name"
-                    {...userFamilyName}
-                  />
-                </Grid>
-              </Grid>
-              <br />
-              <Grid item xs={12}>
+        <Grid container>
+          <form onSubmit={handleAddClient} autoComplete="off">
+            <Grid item>
+              <FormControl fullWidth>
                 <TextField
+                  autoComplete="given-name"
+                  name="addUserGivenName"
                   required
                   fullWidth
-                  id="add-username"
-                  label="username"
-                  name="addUsername"
-                  autoComplete="username"
-                  size="25"
-                  value={username}
-                  onChange={(e) => wrappedSetUsername(e.target.value)}
+                  id="add-user-given-name"
+                  label="First/given name"
+                  autoFocus
+                  {...userGivenName}
                 />
-              </Grid>
-            </FormControl>
-            <br />
-            <br />
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  id="add-webId"
-                  // label="webid"
-                  name="addWebId"
-                  autoComplete="webid"
-                  size="25"
-                  type="text"
-                  value={webId}
-                  onChange={(e) => {
-                    setWebId(e.target.value);
-                  }}
-                  placeholder="WebId"
-                  readOnly
-                  InputProps={{
-                    readOnly: true
-                  }}
-                />
-                <Grid container spacing={1}>
-                  <Button variant="text" startIcon={<ContentCopyIcon />}>
-                    Copy
-                  </Button>
-                </Grid>
-              </Grid>
+              </FormControl>
             </Grid>
-            Link copied!
             <br />
-            <DialogActions>
-              <Button
-                variant="outlined"
-                color="error"
-                endIcon={<ClearIcon />}
-                // onClick={() => setShowModal(false)}
-              >
-                CANCEL
-              </Button>
-              <Button
-                variant="outlined"
-                color="primary"
-                endIcon={<CheckIcon />}
-                // onClick={() => setShowModal(false)}
-                // sx={{ marginLeft: '1rem' }}
-                type="submit"
-                disabled={state.processing}
-              >
-                ADD CLIENT
-              </Button>
-            </DialogActions>
+            <Grid item>
+              <TextField
+                required
+                fullWidth
+                id="add-user-last-name"
+                label="Last/family name"
+                name="addUserFamilyName"
+                autoComplete="family-name"
+                {...userFamilyName}
+              />
+            </Grid>
+            <br />
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="add-username"
+                label="username"
+                name="addUsername"
+                autoComplete="username"
+                value={username}
+                onChange={(e) => wrappedSetUsername(e.target.value)}
+              />
+            </Grid>
+            <br />
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="add-webId"
+                name="addWebId"
+                autoComplete="webid"
+                type="text"
+                value={webId}
+                onChange={(e) => {
+                  setWebId(e.target.value);
+                }}
+                placeholder="WebId"
+              />
+            </Grid>
+            <br />
+            <Grid>
+              <DialogActions>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  endIcon={<ClearIcon />}
+                  onClick={() => setShowModal(false)}
+                >
+                  CANCEL
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  endIcon={<CheckIcon />}
+                  // onClick={() => setShowModal(false)}
+                  type="submit"
+                  disabled={state.processing}
+                >
+                  ADD CLIENT
+                </Button>
+              </DialogActions>
+            </Grid>
           </form>
-        </Box>
+        </Grid>
       </FormSection>
     </Dialog>
   );
