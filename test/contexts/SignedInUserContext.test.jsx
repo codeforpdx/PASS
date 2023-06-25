aff2340996ffe79234ece54edfc403e6db877c09
Ka Hung Lee <lee.kahung414@gmail.com>
2023-06-25 08:12:47 -0700

    Updated snapshot and unit test for SingedInUserContext

--- test/contexts/SignedInUserContext.test.jsx
+++ test/contexts/SignedInUserContext.test.jsx
@@ -55,7 +55,8 @@ describe('SignedInUserContext', () => {
     useSession.mockReturnValue({
       session: {
         info: {
-          isLoggedIn: true
+          isLoggedIn: true,
+          webId: 'https://example.com/profile/card#me'
         }
       }
     });
