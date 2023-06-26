7ea44882344239afc48f7122f3a9b85d8b4c0325
Russell Fraze <russell.fraze@gmail.com>
2023-06-26 10:09:16 -0700

    fix unexpected console statement

--- src/components/Clients/ClientListTable.jsx
+++ src/components/Clients/ClientListTable.jsx
@@ -30,8 +30,6 @@ const ClientListTable = ({ statusType, defaultMessage }) => {
   const { state, dispatch } = useStatusNotification();
   const { userListObject } = useContext(UserListContext);
 
-  console.log(userListObject)
-
   function comparePerson(a, b) {
     if (a.familyName[0].toLowerCase() < b.familyName[0].toLowerCase()) {
       return -1;
