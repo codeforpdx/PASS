822510fce1d1455d42fc9fa7073e4d7343e8ebd2
Andy <awilliamscomposing@gmail.com>
2023-06-30 19:08:10 -0700

    Removing Box component

--- src/components/Form/UploadDocumentModal.jsx
+++ src/components/Form/UploadDocumentModal.jsx
@@ -1,7 +1,6 @@
 // React Imports
 import React, { useState, useContext } from 'react';
 // Material UI Imports
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import ClearIcon from '@mui/icons-material/Clear';
@@ -106,103 +105,98 @@ const UploadDocumentModal = ({ showModal, setShowModal }) => {
         statusType="Upload status"
         defaultMessage="To be uploaded..."
       >
-        <Box display="flex" justifyContent="center">
-          <form onSubmit={handleDocUpload} autoComplete="off">
-            <FormControlLabel
-              control={<Checkbox />}
-              label="Verify file on upload"
-              id="verify-checkbox"
-              value={verifyFile}
-              checked={verifyFile}
-              onChange={() => setVerifyFile(!verifyFile)}
-            />
-            <DocumentSelection
-              htmlForAndIdProp="upload-doc"
-              handleDocType={handleDocType}
-              docType={docType}
-            />
-            <br />
-            <FormControl>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                  name="date"
-                  format="MM/DD/YYYY"
-                  label="Expiration Date"
-                  value={expireDate}
-                  onChange={(newExpireDate) => setExpireDate(newExpireDate)}
-                  type="date"
-                />
-                <FormHelperText>MM/DD/YYYY</FormHelperText>
-              </LocalizationProvider>
-            </FormControl>
-            <br />
-            <br />
-            <FormControl fullWidth>
-              <TextField
-                name="description"
-                multiline
-                rows={4}
-                label="Enter Description"
-                value={docDescription}
-                onChange={(newDocDescription) => setDocDescription(newDocDescription.target.value)}
-                placeholder="Add a description here"
+        <form onSubmit={handleDocUpload} autoComplete="off">
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Verify file on upload"
+            id="verify-checkbox"
+            value={verifyFile}
+            checked={verifyFile}
+            onChange={() => setVerifyFile(!verifyFile)}
+          />
+          <DocumentSelection
+            htmlForAndIdProp="upload-doc"
+            handleDocType={handleDocType}
+            docType={docType}
+          />
+          <br />
+          <FormControl fullWidth>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                name="date"
+                format="MM/DD/YYYY"
+                label="Expiration Date"
+                value={expireDate}
+                onChange={(newExpireDate) => setExpireDate(newExpireDate)}
+                type="date"
               />
-            </FormControl>
-            <br />
-            <br />
-            <FormControl fullWidth>
-              <Button
-                variant="contained"
-                component="label"
-                color="primary"
-                id="upload-doctype"
-                name="uploadDoctype"
-                onChange={(e) => setFile(e.target.files[0])}
-                required
-                startIcon={<SearchIcon />}
-              >
-                Choose file
-                <input type="file" hidden accept=".pdf, .docx, .doc, .txt, .rtf, .gif, .png" />
-              </Button>
-              <FormHelperText
-                sx={{
-                  width: '200px',
-                  whiteSpace: 'nowrap',
-                  overflow: 'hidden',
-                  textOverflow: 'ellipsis'
-                }}
-              >
-                File to upload: {file ? file.name : 'No file selected'}
-              </FormHelperText>
-            </FormControl>
-            <br />
-            <FormControl fullWidth>
-              <Button
-                variant="contained"
-                disabled={state.processing || !file}
-                type="submit"
-                color="primary"
-                startIcon={<FileUploadIcon />}
-              >
-                Upload file
-              </Button>
-            </FormControl>
-            <br />
-            <br />
-            {/* TODO: Determine whether we want a pop-up warning for the user to confirm this action */}
-            <FormControl fullWidth>
-              <Button
-                variant="contained"
-                type="button"
-                color="secondary"
-                onClick={clearInputFields}
-                startIcon={<ClearIcon />}
-              >
-                Clear Form
-              </Button>
-            </FormControl>
-          </form>
-        </Box>
+              <FormHelperText>MM/DD/YYYY</FormHelperText>
+            </LocalizationProvider>
+          </FormControl>
+          <br />
+          <br />
+          <FormControl fullWidth>
+            <TextField
+              name="description"
+              multiline
+              rows={4}
+              label="Enter Description"
+              value={docDescription}
+              onChange={(newDocDescription) => setDocDescription(newDocDescription.target.value)}
+              placeholder="Add a description here"
+            />
+          </FormControl>
+          <br />
+          <br />
+          <FormControl fullWidth>
+            <Button
+              variant="contained"
+              component="label"
+              color="primary"
+              id="upload-doctype"
+              name="uploadDoctype"
+              onChange={(e) => setFile(e.target.files[0])}
+              required
+              startIcon={<SearchIcon />}
+            >
+              Choose file
+              <input type="file" hidden accept=".pdf, .docx, .doc, .txt, .rtf, .gif, .png" />
+            </Button>
+            <FormHelperText
+              sx={{
+                width: '200px',
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis'
+              }}
+            >
+              File to upload: {file ? file.name : 'No file selected'}
+            </FormHelperText>
+            <Button
+              variant="contained"
+              disabled={state.processing || !file}
+              type="submit"
+              color="primary"
+              startIcon={<FileUploadIcon />}
+            >
+              Upload file
+            </Button>
+          </FormControl>
+          <br />
+          <br />
+          {/* TODO: Determine whether we want a pop-up warning for the user to confirm this action */}
+          <FormControl fullWidth>
+            <Button
+              variant="contained"
+              type="button"
+              color="secondary"
+              onClick={clearInputFields}
+              startIcon={<ClearIcon />}
+            >
+              Clear Form
+            </Button>
+          </FormControl>
+        </form>
       </FormSection>
     </Dialog>
   );
