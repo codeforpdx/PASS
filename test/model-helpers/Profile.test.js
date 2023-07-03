c12c1e30df9f8987f6eb8171293209bc09443e8e
Ka Hung Lee <lee.kahung414@gmail.com>
2023-07-02 20:20:08 -0700

    Switching to using full mock Dataset and Thing for Profile.test.js

--- test/model-helpers/Profile.test.js
+++ test/model-helpers/Profile.test.js
@@ -1,7 +1,9 @@
+import * as solidClient from '@inrupt/solid-client';
 import { beforeEach, afterEach, describe, expect, vi, it } from 'vitest';
 import { fetchProfileInfo } from '../../src/model-helpers';
 
 let session = {};
+const mockWebId = 'https://example.com/pod/profile/card#me';
 
 vi.mock('@inrupt/solid-client');
 
@@ -10,7 +12,7 @@ describe('fetchProfileInfo', () => {
     session = {
       fetch: vi.fn(),
       info: {
-        webId: 'https://newtestuser2.opencommons.net/profile/card#me'
+        webId: mockWebId
       }
     };
   });
@@ -19,6 +21,22 @@ describe('fetchProfileInfo', () => {
   });
 
   it('returns expected properties for profileData object', async () => {
+    vi.spyOn(solidClient, 'getWebIdDataset').mockResolvedValue(
+      solidClient.mockSolidDatasetFrom(mockWebId)
+    );
+    vi.spyOn(solidClient, 'getThing').mockReturnValue({
+      predicates: {
+        'http://www.w3.org/1999/02/22-rdf-syntax-ns#type': {
+          namedNodes: ['http://xmlns.com/foaf/0.1/Person']
+        },
+        'http://www.w3.org/ns/solid/terms#oidcIssuer': {
+          namedNodes: ['https://example.com/']
+        }
+      },
+      type: 'Subject',
+      url: 'https://example.com/pod/profile/card#me'
+    });
+
     const results = await fetchProfileInfo(session);
 
     expect(results).toHaveProperty('profileInfo.profileName');
