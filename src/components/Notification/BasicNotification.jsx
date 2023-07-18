bb651a7da288e2f42297e9c4c90b6e23ed32990f
Russell Fraze <russell.fraze@gmail.com>
2023-07-18 12:01:20 -0700

    reusable basic notification added

---
+++ src/components/Notification/BasicNotification.jsx
@@ -0,0 +1,23 @@
+import React, {forwardRef} from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const BasicSnackbar = ({open, onClose, severity, message}) => {
+ 
+
+  return (
+    <>
+      <Snackbar open={open} autoHideDuration={6000} onClose={onClose}>
+        <Alert onClose={onClose} severity={severity} sx={{ width: '100%' }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </>
+  );
+}
+
+export default BasicSnackbar
