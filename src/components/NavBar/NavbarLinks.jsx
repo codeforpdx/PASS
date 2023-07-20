fa48256bd020928e42f3f5272f76ac8096b2c25a
veganedge <scottxbrown@yahoo.com>
2023-07-20 15:22:08 -0700

    removing second dropdown menu in navbar for tabs/routes (no longer necessary with less routes)

--- src/components/NavBar/NavbarLinks.jsx
+++ src/components/NavBar/NavbarLinks.jsx
@@ -1,15 +1,10 @@
 // React Imports
-import React, { useState } from 'react';
+import React from 'react';
 // React Router Imports
 import { NavLink, useLocation } from 'react-router-dom';
 // Material UI Imports
-import Button from '@mui/material/Button';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
-import { useTheme } from '@mui/material/styles';
 
 /**
  * NavbarLinks Component - The sub-component used in NavBar to render the
@@ -20,8 +15,6 @@ import { useTheme } from '@mui/material/styles';
  */
 
 const NavbarLinks = () => {
-  const theme = useTheme();
-
   // Tabs workaround to match route on login
   let location = useLocation().pathname.slice(6);
   if (location === '') {
@@ -34,95 +27,26 @@ const NavbarLinks = () => {
     { label: 'Documents', path: '/PASS/documents' }
   ];
 
-  // Navigate To... button and menu (small screens)
-  const [anchorEl, setAnchorEl] = useState(null);
-  const openNavigateToMenu = Boolean(anchorEl);
-  const handleNavigateToButtonClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleNavigateToMenuClose = () => {
-    setAnchorEl(null);
-  };
-
   return (
-    <>
-      {/* will show on small screen (mobile view) */}
-      <Button
-        id="mobile-navigate-menu-button"
-        aria-controls={openNavigateToMenu ? 'mobile-navigate-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={openNavigateToMenu ? 'true' : undefined}
-        variant="contained"
-        disableElevation
-        onClick={handleNavigateToButtonClick}
-        endIcon={<KeyboardArrowDownIcon />}
-        sx={{ display: { xs: 'flex', md: 'none' } }}
-      >
-        Navigate To...
-      </Button>
-      <Menu
-        elevation={0}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right'
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right'
-        }}
-        id="mobile-navigate-menu"
-        MenuListProps={{
-          'aria-labelledby': 'mobile-navigate-menu-button'
-        }}
-        anchorEl={anchorEl}
-        open={openNavigateToMenu}
-        onClose={handleNavigateToMenuClose}
-        sx={{ backgroundColor: 'rgba(1, 121, 105, 0.2)' }}
-      >
-        {routesArray.map((item) => {
-          const currentRoute = useLocation().pathname.includes(item.path);
-          return (
-            <MenuItem key={item.label} disableTouchRipple component={Button} sx={{ width: '100%' }}>
-              <NavLink
-                to={item.path}
-                end
-                style={{
-                  fontWeight: currentRoute ? 'bold' : '',
-                  color: currentRoute ? theme.palette.tertiary.main : theme.palette.primary.main,
-                  textDecoration: 'none'
-                }}
-                onClick={handleNavigateToMenuClose}
-              >
-                {item.label}
-              </NavLink>
-            </MenuItem>
-          );
-        })}
-      </Menu>
-
-      {/* will show on larger screen (tablet/desktop view) */}
-      <Tabs
-        value={
-          routesArray.map((route) => route.label.toLowerCase()).includes(location)
-            ? location
-            : false
-        }
-        textColor="inherit"
-        indicatorColor="secondary"
-        aria-label="tabs"
-        sx={{ display: { xs: 'none', md: 'flex' } }}
-      >
-        {routesArray.map((item) => (
-          <Tab
-            key={`${item.label}Tab`}
-            value={item.label.toLowerCase()}
-            label={item.label}
-            component={NavLink}
-            to={item.path}
-          />
-        ))}
-      </Tabs>
-    </>
+    <Tabs
+      value={
+        routesArray.map((route) => route.label.toLowerCase()).includes(location) ? location : false
+      }
+      textColor="inherit"
+      indicatorColor="secondary"
+      aria-label="tabs"
+      sx={{ display: { xs: 'none', md: 'flex' } }}
+    >
+      {routesArray.map((item) => (
+        <Tab
+          key={`${item.label}Tab`}
+          value={item.label.toLowerCase()}
+          label={item.label}
+          component={NavLink}
+          to={item.path}
+        />
+      ))}
+    </Tabs>
   );
 };
 
