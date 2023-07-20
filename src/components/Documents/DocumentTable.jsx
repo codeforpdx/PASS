c41e8b8d4b9e7973d326eaae0dff6de8f5076b39
Ka Hung Lee <lee.kahung414@gmail.com>
2023-07-20 03:35:12 -0700

    Created new UserDocumentListContext exclusively for user documents in profile page; Created new prop for user='personal' to indicate user documents context for UploadDocumentModal, DocumentTable, and DocumentTableRow components

--- src/components/Documents/DocumentTable.jsx
+++ src/components/Documents/DocumentTable.jsx
@@ -9,7 +9,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 // Component Imports
-import { DocumentListContext } from '../../contexts';
+import { DocumentListContext, UserDocumentListContext } from '../../contexts';
 import { StyledTableCell } from '../Table/TableStyles';
 import DocumentTableRow from './DocumentTableRow';
 import { EmptyListNotification, LoadingAnimation } from '../Notification';
@@ -19,11 +19,13 @@ import { EmptyListNotification, LoadingAnimation } from '../Notification';
  *
  * @memberof Documents
  * @name DocumentTable
+ * @param user
  * @returns {React.ReactElement} a table of documents
  */
 
-const DocumentTable = () => {
-  const { documentListObject, loadingDocuments } = useContext(DocumentListContext);
+const DocumentTable = ({ user }) => {
+  const { documentListObject, loadingDocuments } =
+    user === 'personal' ? useContext(UserDocumentListContext) : useContext(DocumentListContext);
   const columnTitlesArray = [
     'Name',
     'Type',
@@ -50,7 +52,7 @@ const DocumentTable = () => {
           </TableHead>
           <TableBody>
             {documentListObject?.docList.map((document) => (
-              <DocumentTableRow key={document.name} document={document} />
+              <DocumentTableRow key={document.name} document={document} user={user} />
             ))}
           </TableBody>
         </Table>
