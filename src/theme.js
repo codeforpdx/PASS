92b1a371465d1a2b106ca03e00c5a9d3c660df22
Russell Fraze <russell.fraze@gmail.com>
2023-08-01 14:56:24 -0700

    comment out purple theme

--- src/theme.js
+++ src/theme.js
@@ -32,36 +32,36 @@ const greenTheme = createTheme({
   }
 })
 
-const purpleTheme = createTheme({
-  typography: { fontFamily: 'Roboto, sans-serif' },
-  palette: {
-    primary: {
-      light: '#039686',
-      main: '#6750a4',
-      dark: '#004d3e',
-      slight: '#eaddff',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#b32126',
-      main: '#625b71',
-      dark: '#790111',
-      contrastText: '#fff'
-    },
-    tertiary: {
-      light: '#dbc584',
-      main: '#7d5260',
-      dark: '#dbb032',
-      contrastText: '#fff'
-    },
-    status: {
-      danger: '#b3261e'
-    },
-    neutral: {
-      main: '#64748B',
-      contrastText: '#fff'
-    }
-  }
-});
+// const purpleTheme = createTheme({
+//   typography: { fontFamily: 'Roboto, sans-serif' },
+//   palette: {
+//     primary: {
+//       light: '#039686',
+//       main: '#6750a4',
+//       dark: '#004d3e',
+//       slight: '#eaddff',
+//       contrastText: '#fff'
+//     },
+//     secondary: {
+//       light: '#b32126',
+//       main: '#625b71',
+//       dark: '#790111',
+//       contrastText: '#fff'
+//     },
+//     tertiary: {
+//       light: '#dbc584',
+//       main: '#7d5260',
+//       dark: '#dbb032',
+//       contrastText: '#fff'
+//     },
+//     status: {
+//       danger: '#b3261e'
+//     },
+//     neutral: {
+//       main: '#64748B',
+//       contrastText: '#fff'
+//     }
+//   }
+// });
 
 export default greenTheme;
