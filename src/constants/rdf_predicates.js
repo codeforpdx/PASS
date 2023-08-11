1943f5c93df1d2d3437b12fc75c36667c8400bab
Ka Hung Lee <lee.kahung414@gmail.com>
2023-08-11 14:48:28 -0700

    Switch existing DOB schema from schema.org to hmis-interop

--- src/constants/rdf_predicates.js
+++ src/constants/rdf_predicates.js
@@ -4,7 +4,8 @@ const RDF_PREDICATES = {
   ...SCHEMA_INRUPT,
   about: 'https://schema.org/about',
   uploadDate: 'https://schema.org/uploadDate',
-  dateOfBirth: 'https://schema.org/birthDate',
+  dateOfBirth:
+    'https://github.com/hmis-interop/logical-model/blob/FY2022-v1.0.0/src/logical-model.n3.owl#DOB',
   additionalType: 'https://schema.org/additionalType',
   conditionsOfAccess: 'https://schema.org/conditionsOfAccess',
   expires: 'https://schema.org/expires',
