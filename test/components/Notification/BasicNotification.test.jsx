985ab72965c0f3407e7d6cea0f05cc9b3fc7cf71
Russell Fraze <russell.fraze@gmail.com>
2023-08-15 11:39:58 -0700

    start to delete notificaiton test added

--- test/components/Notification/BasicNotification.test.jsx
+++ test/components/Notification/BasicNotification.test.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {
+  queryByText,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved
+} from '@testing-library/react';
 import { expect, it } from 'vitest';
+import { NotificationContext } from '../../../src/contexts/NotificationContext';
 import BasicNotification from '../../../src/components/Notification/BasicNotification';
 
 it('renders correctly', () => {
@@ -16,4 +23,31 @@ it('renders correctly', () => {
   expect(screen.getByText('my test message')).not.toBeNull();
 });
 
-// it('is deleted after the specified time', () => {});
+beforeEach(() => {
+  vi.useFakeTimers();
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
+
+
+
+// it('is deleted after 8 seconds', () => {
+//   const remove = vi.fn();
+
+//   render(
+//     <NotificationContext.Provider value={{ dispatch }}>
+//       <BasicNotification
+//         severity="success"
+//         message="my test message"
+//         id={123456}
+//         dataTestId="test-id"
+//       />
+//     </NotificationContext.Provider>
+//   );
+
+//   vi.advanceTimersByTime(8000);
+
+//   expect(screen.getByText('my test message')).toBeNull();
+// });
