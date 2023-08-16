af862751b607893f377fa2b41c333cbae9afa395
Russell Fraze <russell.fraze@gmail.com>
2023-08-16 14:48:30 -0700

    two tests added to basic notification test

--- test/components/Notification/BasicNotification.test.jsx
+++ test/components/Notification/BasicNotification.test.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { expect, it } from 'vitest';
+import { act } from 'react-dom/test-utils';
 import BasicNotification from '../../../src/components/Notification/BasicNotification';
+import { NotificationContext } from '../../../src/contexts/NotificationContext';
 
 it('renders correctly', () => {
   render(
@@ -16,29 +18,52 @@ it('renders correctly', () => {
   expect(screen.getByText('my test message')).not.toBeNull();
 });
 
-// beforeEach(() => {
-//   vi.useFakeTimers();
-// });
+beforeEach(() => {
+  vi.useFakeTimers();
+});
 
-// afterEach(() => {
-//   vi.useRealTimers();
-// });
+afterEach(() => {
+  vi.useRealTimers();
+});
 
-// it('is deleted after 8 seconds', () => {
-//   const remove = vi.fn();
+it('calls remove after 8 seconds', () => {
+  const remove = vi.fn();
 
-//   render(
-//     <NotificationContext.Provider value={{ dispatch }}>
-//       <BasicNotification
-//         severity="success"
-//         message="my test message"
-//         id={123456}
-//         dataTestId="test-id"
-//       />
-//     </NotificationContext.Provider>
-//   );
+  render(
+    <NotificationContext.Provider value={{ remove }}>
+      <BasicNotification
+        severity="success"
+        message="my test message"
+        id={123456}
+        dataTestId="test-id"
+      />
+    </NotificationContext.Provider>
+  );
 
-//   vi.advanceTimersByTime(8000);
+  act(() => {
+    vi.advanceTimersByTime(8000);
+  });
 
-//   expect(screen.getByText('my test message')).toBeNull();
-// });
+  expect(remove).toHaveBeenCalledOnce();
+});
+
+it('calls remove with the correct id', () => {
+  const remove = vi.fn();
+
+  render(
+    <NotificationContext.Provider value={{ remove }}>
+      <BasicNotification
+        severity="success"
+        message="my test message"
+        id={123456}
+        dataTestId="test-id"
+      />
+    </NotificationContext.Provider>
+  );
+
+  act(() => {
+    vi.advanceTimersByTime(8000);
+  });
+
+  expect(remove).toHaveBeenCalledWith(123456);
+});
