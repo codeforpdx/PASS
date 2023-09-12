67da698e192ac24661db25cf0830380a16210b86
Ka Hung Lee <lee.kahung414@gmail.com>
2023-09-11 17:25:16 -0700

    Included missing BroswerRouter inside unit tests and ordered them similar to how it's ordered in App.jsx

--- test/components/NavBar/NavbarLoggedOut.test.jsx
+++ test/components/NavBar/NavbarLoggedOut.test.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { expect, it } from 'vitest';
+import { BrowserRouter } from 'react-router-dom';
 import NavbarLoggedOut from '../../../src/components/NavBar/NavbarLoggedOut';
 
 it('renders login button when user is logged out', () => {
-  const { queryByLabelText } = render(<NavbarLoggedOut />);
+  const { queryByLabelText } = render(
+    <BrowserRouter>
+      <NavbarLoggedOut />
+    </BrowserRouter>
+  );
 
   const logo = queryByLabelText('logo');
   const loginButton = queryByLabelText('Login Button');
