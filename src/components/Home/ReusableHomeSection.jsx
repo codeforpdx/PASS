909ff60850818665067efa51517ba7e25b4e006e
Hauvert <hpachec1@gmail.com>
2023-09-12 18:59:41 -0700

    Revamped Homepage with fixed issues

--- src/components/Home/ReusableHomeSection.jsx
+++ src/components/Home/ReusableHomeSection.jsx
@@ -4,54 +4,52 @@ import React from 'react';
 import { Box, Typography, Button } from '@mui/material';
 
 const ReusableHomeSection = ({ src, alt, title, description, button, href, label }) => {
-    (
-        <>
-            <Box
-                component="img"
-                src={src}
-                alt={alt}
-                sx={{
-                    width: { xs: '100%', sm: '75%', md: '65%' }
-                }}
-            />
-            <Typography
-                sx={{
-                    color: 'Purple.dark',
-                    fontSize: { xs: '2rem', sm: '3.5rem', md: '3rem' },
-                    textAlign: 'center',
-                    marginBottom: '24px'
-                }}
-            >
-                <strong>{title}</strong>
-            </Typography>
-            <Typography
-                sx={{
-                    color: 'Purple.main',
-                    fontSize: { sm: '1.5rem', md: '1.5rem' },
-                    width: { md: '75%' },
-                    marginBottom: '24px'
-                }}
-            >
-                {description}
-            </Typography>
-            {button && (
-                <Button
-                    variant="contained"
-                    href={href}
-                    aria-label={label}
-                    sx={{
-                        my: '1rem',
-                        color: 'Purple.main',
-                        backgroundColor: 'Purple.light',
-                        width: { xs: 1, sm: 1 / 3, md: 1 / 5 },
-                        borderRadius: '25px'
-                    }}
-                >
-                    {button}
-                </Button>
-            )}
-        </>
-    );
+  <>
+    <Box
+      component="img"
+      src={src}
+      alt={alt}
+      sx={{
+        width: { xs: '100%', sm: '75%', md: '65%' }
+      }}
+    />
+    <Typography
+      sx={{
+        color: 'Purple.dark',
+        fontSize: { xs: '2rem', sm: '3.5rem', md: '3rem' },
+        textAlign: 'center',
+        marginBottom: '24px'
+      }}
+    >
+      <strong>{title}</strong>
+    </Typography>
+    <Typography
+      sx={{
+        color: 'Purple.main',
+        fontSize: { sm: '1.5rem', md: '1.5rem' },
+        width: { md: '75%' },
+        marginBottom: '24px'
+      }}
+    >
+      {description}
+    </Typography>
+    {button && (
+      <Button
+        variant="contained"
+        href={href}
+        aria-label={label}
+        sx={{
+          my: '1rem',
+          color: 'Purple.main',
+          backgroundColor: 'Purple.light',
+          width: { xs: 1, sm: 1 / 3, md: 1 / 5 },
+          borderRadius: '25px'
+        }}
+      >
+        {button}
+      </Button>
+    )}
+  </>;
 };
 
 export default ReusableHomeSection;
--- src/components/Home/ReusableKeyFeatures.jsx
+++ src/components/Home/ReusableKeyFeatures.jsx
@@ -1,45 +1,43 @@
 // React Imports
-import React from 'react'
+import React from 'react';
 // Material UI Imports
-import { Box } from '@mui/system'
+import { Box } from '@mui/system';
 import { Typography } from '@mui/material';
 
-const ReusableKeyFeatures = ({icon, title, description}) =>{
-    (
-        <>
-        <Box
+const ReusableKeyFeatures = ({ icon, title, description }) => {
+  <>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: '14px',
+        textAlign: 'center'
+      }}
+    >
+      {icon}
+      <Typography
         sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            marginBottom: "14px",
-            textAlign: "center"
-            
+          marginLeft: '8px',
+          color: 'Purple.main',
+          fontSize: { xs: '1rem', sm: '1.25rem' }
         }}
-        >
-            {icon}
-            <Typography
-            sx={{
-                marginLeft: "8px",
-                color: "Purple.main",
-                fontSize: { xs: "1rem", sm: "1.25rem"}
-            }}
-            ><strong>{title}</strong>
-            </Typography>
-        </Box>
-        <Typography
-        sx={{
-            width: { xs:"90%", md: "65%"},
-            textAlign: "center",
-            fontSize: { xs: ".85rem", sm: "1rem", md: "1.25rem"},
-            color: "Purple.text",
-            marginBottom: "50px"
-        }}
-        >
-            {description}
-        </Typography>
-        </>
-    )
-}
+      >
+        <strong>{title}</strong>
+      </Typography>
+    </Box>
+    <Typography
+      sx={{
+        width: { xs: '90%', md: '65%' },
+        textAlign: 'center',
+        fontSize: { xs: '.85rem', sm: '1rem', md: '1.25rem' },
+        color: 'Purple.text',
+        marginBottom: '50px'
+      }}
+    >
+      {description}
+    </Typography>
+  </>;
+};
 
-export default ReusableKeyFeatures
+export default ReusableKeyFeatures;
