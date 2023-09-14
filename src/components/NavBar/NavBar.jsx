81caba221368f3bc96d5ed8ff83bc529a259f186
veganedge <scottxbrown@yahoo.com>
2023-09-14 15:45:26 -0700

    replacing logoutModal with confirmationModal

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ import { useNotification, useSession } from '@hooks';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 // Component Import
-import { ConfirmationModal, LogoutModal } from '../Modals';
+import { ConfirmationModal } from '../Modals';
 import NavbarDesktop from './NavbarDesktop';
 import NavbarLoggedOut from './NavbarLoggedOut';
 import NavbarMobile from './NavbarMobile';
@@ -25,20 +25,11 @@ const NavBar = () => {
   const isLargeScreen = useMediaQuery(theme.breakpoints.up('md'));
   const { addNotification } = useNotification();
 
-  // state for LogoutModal component
-  const [showConfirmation, setShowConfirmation] = useState(false);
-
-  // Event handler for logging out of SOLID POD and removing items from localStorage
-  const handleLogout = () => {
-    localStorage.clear();
-    setShowConfirmation(false);
-  };
-
   // states for ConfirmationModal
   const [processing, setProcessing] = useState(false);
   const [showConfirmationModal, setShowConfirmationModal] = useState(false);
 
-  const handleLogout2 = async () => {
+  const handleLogout = async () => {
     setProcessing(true);
 
     try {
@@ -54,22 +45,18 @@ const NavBar = () => {
 
   return session.info.isLoggedIn ? (
     <>
-      {isSmallScreen && <NavbarMobile setShowConfirmation={setShowConfirmation} />}
-      {isLargeScreen && <NavbarDesktop setShowConfirmation={setShowConfirmation} />}
+      {isSmallScreen && <NavbarMobile setShowConfirmation={setShowConfirmationModal} />}
+      {isLargeScreen && <NavbarDesktop setShowConfirmation={setShowConfirmationModal} />}
       <ConfirmationModal
         showConfirmationModal={showConfirmationModal}
         setShowConfirmationModal={setShowConfirmationModal}
         title="Log out?"
         text="This will log you out of your pod. Are you sure?"
-        confirmButtonFunction={handleLogout2}
+        confirmButtonFunction={handleLogout}
         confirmButtonText="YES"
         processing={processing}
+        isLogout
       />
-      {/* <LogoutModal
-        showConfirmation={showConfirmation}
-        setShowConfirmation={setShowConfirmation}
-        handleLogout={handleLogout}
-      /> */}
     </>
   ) : (
     <NavbarLoggedOut />
