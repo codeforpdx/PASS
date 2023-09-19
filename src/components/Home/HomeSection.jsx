e64dfbfc8deae69139575e376751c4a7e4f33301
Hauvert <hpachec1@gmail.com>
2023-09-18 21:52:04 -0700

    made most suggested requets

--- src/components/Home/HomeSection.jsx
+++ src/components/Home/HomeSection.jsx
@@ -12,63 +12,63 @@ import Button from '@mui/material/Button';
  */
 
 const HomeSection = ({
-  src,
-  alt,
-  title,
-  description,
-  button,
-  href,
-  label,
-  isReallySmallScreen
+    src,
+    alt,
+    title,
+    description,
+    button,
+    href,
+    label,
+    isReallySmallScreen
 }) => {
-  return (
-    <>
-      <Box
-        component="img"
-        src={src}
-        alt={alt}
-        sx={{
-          width: isReallySmallScreen ? 1 : 2 / 4
-        }}
-      />
-      <Typography
-        variant={isReallySmallScreen ? 'h3' : 'h2'}
-        sx={{
-          color: 'primary.main',
-          textAlign: 'center',
-          marginBottom: '24px'
-        }}
-      >
-        <strong>{title}</strong>
-      </Typography>
-      <Typography
-        variant={isReallySmallScreen ? 'h6' : 'h5'}
-        sx={{
-          color: 'primary.dark',
-          width: isReallySmallScreen ? '100%' : '85%',
-          marginBottom: '24px',
-          textAlign: 'center'
-        }}
-      >
-        {description}
-      </Typography>
-      {button && (
-        <Button
-          variant="contained"
-          href={href}
-          aria-label={label}
-          sx={{
-            my: '1rem',
-            backgroundColor: 'primary.light',
-            width: isReallySmallScreen ? 1 : 1 / 4,
-            borderRadius: '25px'
-          }}
-        >
-          {button}
-        </Button>
-      )}
-    </>
-  );
+    return (
+        <>
+            <Box
+                component="img"
+                src={src}
+                alt={alt}
+                sx={{
+                    width: isReallySmallScreen ? 1 : 2 / 4
+                }}
+            />
+            <Typography
+                variant={isReallySmallScreen ? 'h3' : 'h2'}
+                sx={{
+                    color: 'primary.main',
+                    textAlign: 'center',
+                    marginBottom: '24px'
+                }}
+            >
+                <strong>{title}</strong>
+            </Typography>
+            <Typography
+                variant={isReallySmallScreen ? 'h6' : 'h5'}
+                sx={{
+                    color: 'primary.dark',
+                    width: isReallySmallScreen ? '100%' : '85%',
+                    marginBottom: '24px',
+                    textAlign: 'center'
+                }}
+            >
+                {description}
+            </Typography>
+            {button && (
+                <Button
+                    variant="contained"
+                    href={href}
+                    aria-label={label}
+                    sx={{
+                        my: '1rem',
+                        backgroundColor: 'primary.light',
+                        width: isReallySmallScreen ? 1 : 1 / 4,
+                        borderRadius: '25px'
+                    }}
+                >
+                    {button}
+                </Button>
+            )}
+        </>
+    );
 };
 
 export default HomeSection;
