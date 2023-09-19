0b6aa6090cec5d1c5283de878998caaac545d10b
Hauvert <hpachec1@gmail.com>
2023-09-18 22:12:17 -0700

    made most suggested requets

--- src/components/Home/HomeSection.jsx
+++ src/components/Home/HomeSection.jsx
@@ -5,64 +5,62 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
-
-
 const HomeSection = ({
-    src,
-    alt,
-    title,
-    description,
-    button,
-    href,
-    label,
-    isReallySmallScreen
+  src,
+  alt,
+  title,
+  description,
+  button,
+  href,
+  label,
+  isReallySmallScreen
 }) => (
-        <>
-            <Box
-                component="img"
-                src={src}
-                alt={alt}
-                sx={{
-                    width: isReallySmallScreen ? 1 : 2 / 4
-                }}
-            />
-            <Typography
-                variant={isReallySmallScreen ? 'h3' : 'h2'}
-                sx={{
-                    color: 'primary.main',
-                    textAlign: 'center',
-                    marginBottom: '24px'
-                }}
-            >
-                <strong>{title}</strong>
-            </Typography>
-            <Typography
-                variant={isReallySmallScreen ? 'h6' : 'h5'}
-                sx={{
-                    color: 'primary.dark',
-                    width: isReallySmallScreen ? '100%' : '85%',
-                    marginBottom: '24px',
-                    textAlign: 'center'
-                }}
-            >
-                {description}
-            </Typography>
-            {button && (
-                <Button
-                    variant="contained"
-                    href={href}
-                    aria-label={label}
-                    sx={{
-                        my: '1rem',
-                        backgroundColor: 'primary.light',
-                        width: isReallySmallScreen ? 1 : 1 / 4,
-                        borderRadius: '25px'
-                    }}
-                >
-                    {button}
-                </Button>
-            )}
-        </>
-    );
+  <>
+    <Box
+      component="img"
+      src={src}
+      lt={alt}
+      sx={{
+        width: isReallySmallScreen ? 1 : 2 / 4
+      }}
+    />
+    <Typography
+      variant={isReallySmallScreen ? 'h3' : 'h2'}
+      sx={{
+        color: 'primary.main',
+        textAlign: 'center',
+        marginBottom: '24px'
+      }}
+    >
+      <strong>{title}</strong>
+    </Typography>
+    <Typography
+      variant={isReallySmallScreen ? 'h6' : 'h5'}
+      sx={{
+        color: 'primary.dark',
+        width: isReallySmallScreen ? '100%' : '85%',
+        marginBottom: '24px',
+        textAlign: 'center'
+      }}
+    >
+      {description}
+    </Typography>
+    {button && (
+      <Button
+        variant="contained"
+        href={href}
+        aria-label={label}
+        sx={{
+          my: '1rem',
+          backgroundColor: 'primary.light',
+          width: isReallySmallScreen ? 1 : 1 / 4,
+          borderRadius: '25px'
+        }}
+      >
+        {button}
+      </Button>
+    )}
+  </>
+);
 
 export default HomeSection;
--- src/components/Home/KeyFeatures.jsx
+++ src/components/Home/KeyFeatures.jsx
@@ -1,45 +1,44 @@
 // React Imports
 import React from 'react';
 // Material UI Imports
-import  Box  from '@mui/material/Box';
-import  Typography  from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-
-const KeyFeatures = ( {icon, title, description, isReallySmallScreen} ) => (
-    <>
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          marginBottom: '14px',
-          color: 'primary.main',
-          width: isReallySmallScreen ? 1 : 'full'
-        }}
-      >
-        {icon}
-        <Typography
-          variant={isReallySmallScreen ? 'h5' : 'h4'}
-          sx={{
-            marginLeft: '8px',
-            textAlign: isReallySmallScreen ? 'start' : 'center',
-            width: '100%'
-          }}
-        >
-          <strong>{title}</strong>
-        </Typography>
-      </Box>
+const KeyFeatures = ({ icon, title, description, isReallySmallScreen }) => (
+  <>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        marginBottom: '14px',
+        color: 'primary.main',
+        width: isReallySmallScreen ? 1 : 'full'
+      }}
+    >
+      {icon}
       <Typography
-        variant="body1"
+        variant={isReallySmallScreen ? 'h5' : 'h4'}
         sx={{
-          width: isReallySmallScreen ? 1 : 2 / 3,
+          marginLeft: '8px',
           textAlign: isReallySmallScreen ? 'start' : 'center',
-          color: 'primary.dark',
-          marginBottom: '50px'
+          width: '100%'
         }}
       >
-        {description}
+        <strong>{title}</strong>
       </Typography>
-    </>
-  );
+    </Box>
+    <Typography
+      variant="body1"
+      sx={{
+        width: isReallySmallScreen ? 1 : 2 / 3,
+        textAlign: isReallySmallScreen ? 'start' : 'center',
+        color: 'primary.dark',
+        marginBottom: '50px'
+      }}
+    >
+      {description}
+    </Typography>
+  </>
+);
 
 export default KeyFeatures;
