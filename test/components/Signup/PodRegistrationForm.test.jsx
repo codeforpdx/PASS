b581ea8f47d134a6098351fde89a60a43985c0e0
JP McGarrity <design@jpmcgarrity.com>
2023-09-21 09:56:12 -0700

    issue 408: added test for PodRegistrationForm component

--- test/components/Signup/PodRegistrationForm.test.jsx
+++ test/components/Signup/PodRegistrationForm.test.jsx
@@ -10,21 +10,19 @@ import { expect, it, describe } from 'vitest';
 import PodRegistrationForm from '../../../src/components/Signup/PodRegistrationForm';
 
 describe('PodRegistrationForm', () => {
-  it('Renders the PodRegistrationForm Component', () => {
-    act(() => {
+  it('renders', () => {
       render (
         <Router>
           <PodRegistrationForm />
         </Router>
-      );
-      });
+        );
 
     const textField = screen.getByLabelText('Email');
     const inputElementPassword = screen.getByLabelText('Password');
     const inputElementConfirmPassord = screen.getByLabelText('Confirm Password');
 
-    expect(textField).to.equal('Email');
-    expect(inputElementPassword).to.equal('Password');
-    expect(inputElementConfirmPassord).to.equal('Confirm Password');
+    expect(textField).not.toBeNull();
+    expect(inputElementPassword).not.toBeNull();
+    expect(inputElementConfirmPassord).not.toBeNull();
   });
 });
