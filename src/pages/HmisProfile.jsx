e61d8a335ff398c02b5711d61052a0f7618d8c9e
Tim Standen <timmstanden@gmail.com>
2023-10-15 20:49:59 -0700

    Add MUI MenuList

--- src/pages/HmisProfile.jsx
+++ src/pages/HmisProfile.jsx
@@ -1,21 +1,39 @@
-import { Container } from '@mui/material';
+import { Container, MenuList, MenuItem } from '@mui/material';
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import { HMIS_FORM_LIST } from '@constants';
 
-const HmisProfile = () => (
-  <Container>
-    <nav>
-      <ul>
-        {HMIS_FORM_LIST.map((form) => (
-          <li>
-            <Link to={form.path}>{form.label}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-    <Outlet />
-  </Container>
-);
+const HmisProfile = () => {
+  const location = useLocation();
+
+  const currentForm = location.pathname.split('/').pop();
+
+  return (
+    <Container sx={{ display: 'flex' }}>
+      <Container sx={{ width: '25%' }}>
+        <nav>
+          <MenuList>
+            {HMIS_FORM_LIST.map((form) => (
+              <Link to={form.path} style={{ textDecoration: 'none' }}>
+                <MenuItem
+                  divider
+                  selected={
+                    currentForm === form.path ||
+                    (currentForm === 'hmis_profile' && form.path === '')
+                  }
+                >
+                  {form.label}
+                </MenuItem>
+              </Link>
+            ))}
+          </MenuList>
+        </nav>
+      </Container>
+      <Container>
+        <Outlet />
+      </Container>
+    </Container>
+  );
+};
 
 export default HmisProfile;
