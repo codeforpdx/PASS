71f573b0c20be91158e10800a34f2043bf0a0fed
Milo Fultz <milopfultz@gmail.com>
2023-10-17 18:09:35 -0700

    Fix rebase oopsie

--- test/components/NavBar/NavbarLoggedOut.test.jsx
+++ test/components/NavBar/NavbarLoggedOut.test.jsx
@@ -5,14 +5,14 @@ import { BrowserRouter } from 'react-router-dom';
 import NavbarLoggedOut from '../../../src/components/NavBar/NavbarLoggedOut';
 
 it('renders login button when user is logged out', () => {
-  const { queryByLabelText, queryByRole } = render(
+  const { queryByRole } = render(
     <BrowserRouter>
       <NavbarLoggedOut />
     </BrowserRouter>
   );
 
   const logo = queryByRole('img', { name: /logo$/ });
-  const loginButton = queryByLabelText('Login Button');
+  const loginButton = queryByRole('button');
 
   expect(logo).not.toBeNull();
   expect(loginButton).not.toBeNull();
