61e941c47fd37f3d110087cd7c397c8cd31325d8
Tim Standen <timmstanden@gmail.com>
2023-10-22 16:02:11 -0700

    add more tests

--- test/pages/HmisProfile.test.jsx
+++ test/pages/HmisProfile.test.jsx
@@ -3,14 +3,18 @@ import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { expect, it, describe } from 'vitest';
 import { HmisProfile } from '@pages';
+import { HMIS_FORM_LIST } from '@components/HmisForms';
 
 describe('HMIS Profile', () => {
-  it('renders', () => {
-    const { getByRole } = render(
+  const numLinks = HMIS_FORM_LIST.length;
+
+  it('renders buttons for all forms in HMIS_FORM_LIST', () => {
+    const { getByRole, getAllByRole } = render(
       <BrowserRouter>
         <HmisProfile />
       </BrowserRouter>
     );
     expect(getByRole('navigation')).not.toBeNull();
+    expect(getAllByRole('link').length).toEqual(numLinks);
   });
 });
