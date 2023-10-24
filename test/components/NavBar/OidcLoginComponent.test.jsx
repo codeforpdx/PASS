54a39eb598a8838e73d4f807eb9c6adf5bddddf8
Rio Edwards <rioredwards@gmail.com>
2023-10-24 13:15:04 -0700

    Add new test for OIDC input dropdown

--- test/components/NavBar/OidcLoginComponent.test.jsx
+++ test/components/NavBar/OidcLoginComponent.test.jsx
@@ -18,7 +18,9 @@ vi.mock('../../../src/constants/', async () => {
   return {
     ...actual,
     ENV: {
-      VITE_SOLID_IDENTITY_PROVIDER: 'https://www.testurl.com/'
+      VITE_SOLID_IDENTITY_PROVIDER: 'https://www.testurl.com/',
+      VITE_SUGGESTED_OIDC_OPTIONS:
+        'http://testurl_1.com/, http://testurl_2.com/, http://testurl_3.com/'
     }
   };
 });
@@ -26,12 +28,29 @@ vi.mock('../../../src/constants/', async () => {
 it('sets OIDC provider on login', async () => {
   const user = userEvent.setup();
   const { getByRole } = render(<OidcLoginComponent />);
+
   const input = getByRole('combobox');
   const loginButton = getByRole('button');
+
   await user.clear(input);
   await user.type(input, 'http://oidc.provider.url/');
   expect(input.value).toBe('http://oidc.provider.url/');
+
   await user.click(loginButton);
   expect(login).toBeCalled();
   expect(localStorage.getItem('oidcIssuer')).toBe('http://oidc.provider.url/');
+});
+
+it('displays a list of suggested providers when focused', async () => {
+  const user = userEvent.setup();
+  const { getByRole, getByText } = render(<OidcLoginComponent />);
+
+  const input = getByRole('combobox');
+  await user.click(input);
+
+  const exampleProvider = getByText('http://testurl_2.com/');
+  expect(exampleProvider).not.toBeNull();
+
+  await user.click(exampleProvider);
+  expect(input.value).toBe('http://testurl_2.com/');
 });
