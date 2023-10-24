fc96be6c7c94a4334210e95344880c11c737c2f9
Rio Edwards <rioredwards@gmail.com>
2023-10-23 21:19:53 -0700

    Add tests for new OidcLoginComponent dropdown (display providers, updates input value, and saves to local storage)

--- test/components/NavBar/OidcLoginComponent.test.jsx
+++ test/components/NavBar/OidcLoginComponent.test.jsx
@@ -1,8 +1,7 @@
 import { render, cleanup } from '@testing-library/react';
-import { login } from '@inrupt/solid-client-authn-browser';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { expect, it, vi, afterEach } from 'vitest';
+import { expect, it, vi, afterEach, describe, beforeEach } from 'vitest';
 import OidcLoginComponent from '../../../src/components/NavBar/OidcLoginComponent';
 
 vi.mock('@inrupt/solid-client-authn-browser');
@@ -18,20 +17,47 @@ vi.mock('../../../src/constants/', async () => {
   return {
     ...actual,
     ENV: {
-      VITE_SOLID_IDENTITY_PROVIDER: 'https://www.testurl.com/'
+      VITE_SOLID_IDENTITY_PROVIDER: 'https://www.testurl.com/',
+      VITE_SUGGESTED_OIDC_OPTIONS:
+        'http://localhost:3000/, https://opencommons.net/, https://solidcommunity.net/, https://login.inrupt.com/, https://inrupt.net/'
     }
   };
 });
 
-it('sets OIDC provider on login', async () => {
-  const user = userEvent.setup();
-  const { getByRole } = render(<OidcLoginComponent />);
-  const input = getByRole('combobox');
-  const loginButton = getByRole('button');
-  await user.clear(input);
-  await user.type(input, 'http://oidc.provider.url/');
-  expect(input.value).toBe('http://oidc.provider.url/');
-  await user.click(loginButton);
-  expect(login).toBeCalled();
-  expect(localStorage.getItem('oidcIssuer')).toBe('http://oidc.provider.url/');
+describe('OidcLoginComponent Tests', () => {
+  let user;
+  let input;
+  let getByRole;
+  let getByText;
+  let exampleProvider;
+
+  beforeEach(async () => {
+    user = userEvent.setup();
+    const renderResult = render(<OidcLoginComponent />);
+    getByRole = renderResult.getByRole;
+    getByText = renderResult.getByText;
+
+    input = getByRole('combobox');
+    await user.click(input);
+    exampleProvider = getByText('https://solidcommunity.net/');
+  });
+
+  it('displays a list of suggested providers when focused', () => {
+    expect(exampleProvider).not.toBeNull();
+  });
+
+  it('updates input value when a provider is clicked', async () => {
+    await user.click(exampleProvider);
+    expect(input.value).toBe('https://solidcommunity.net/');
+  });
+
+  it('saves the provider to localStorage on login', async () => {
+    await userEvent.click(input);
+    await userEvent.click(exampleProvider);
+
+    const loginButton = getByRole('button');
+    await userEvent.click(loginButton);
+
+    expect(localStorage.getItem('oidcIssuer')).toBe('https://solidcommunity.net/');
+  });
 });
