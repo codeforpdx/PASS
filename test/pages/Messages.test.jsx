8a64113d45fde6d4ed7f534b93f3717063abdc1f
JP McGarrity <design@jpmcgarrity.com>
2023-11-04 10:08:23 -0700

    bug 454: broken test should render messages

--- test/pages/Messages.test.jsx
+++ test/pages/Messages.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, act, screen } from '@testing-library/react';
 import { it, describe, expect, vi } from 'vitest';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -22,7 +22,20 @@ const MockMessageContextValue = {
       recipient: 'PODMAN'
     }
   ],
-  outboxList: [{}],
+  outboxList: [
+    {
+      message: 'test 2',
+      messageId: '3bf2a18d-0c6a-43e4-9650-992bf4fe7fe8',
+      messageUrl:
+        'http://localhost:3000/pod-test-name-here/PASS/Inbox/RE%3Ateste-20231023-195933.ttl',
+      title: 'RE:teste',
+      uploadDate: '2023-10-23T19:59:31.424Z',
+      readStatus: true,
+      sender: 'PODMAN',
+      senderWebId: 'http://localhost:3000/pod-test-name-here/profile/card#me',
+      recipient: 'PODMAN'
+    }
+  ],
   setInboxList: vi.fn(),
   setOutboxList: vi.fn(),
   loadMessages: vi.fn(),
@@ -58,5 +71,25 @@ describe('Messages Page', () => {
     const { getByText } = render(<MockSignupContexts session={sessionObj} />);
     expect(getByText('Inbox', { exact: true })).not.toBeNull();
     expect(getByText('Outbox', { exact: true })).not.toBeNull();
+  });
+
+  it('should render messages', async () => {
+    const sessionObj = {
+      login: vi.fn(),
+      fetch: vi.fn(),
+      podUrl: 'https://example.com',
+      session: {
+        info: {
+          webId: 'https://example.com/profile/',
+          isLoggedIn: true
+        }
+      }
+    };
+
+    await act(() => {
+      render(<MockSignupContexts session={sessionObj} />);
+      const messages = screen.getByText('test 1');
+      expect(messages).not.toBe(null);
+    });
   });
 });
