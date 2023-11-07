ae7a12c08d9955f6d3b4ae6149ae2b9491b28481
JP McGarrity <design@jpmcgarrity.com>
2023-11-06 16:06:31 -0800

    Enhancement 491: Added more tests for unread messages

--- test/pages/Messages.test.jsx
+++ test/pages/Messages.test.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { it, describe, expect, vi } from 'vitest';
 import { BrowserRouter } from 'react-router-dom';
 import { SessionContext, MessageContext } from '@contexts';
 import { Messages } from '@pages';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
 
 const MockMessageContextValue = {
   inboxList: [
     {
-      message: 'test 1',
+      message: 'test message',
       messageId: '3bf2a18d-0c6a-43e4-9650-992bf4fe7fe7',
       messageUrl:
         'http://localhost:3000/pod-test-name-here/PASS/Inbox/RE%3Ateste-20231023-195931.ttl',
       title: 'RE:test-inbox',
       uploadDate: new Date('2023-10-23T19:59:31.424Z'),
       readStatus: false,
+      sender: 'PODMAN',
+      senderWebId: 'http://localhost:3000/pod-test-name-here/profile/card#me',
+      recipient: 'PODMAN'
+    },
+    {
+      message: 'test 3',
+      messageId: '3bf2a18d-0c6a-43e4-9650-992bf4fe7fe9',
+      messageUrl:
+        'http://localhost:3000/pod-test-name-here/PASS/Inbox/RE%3Ateste-20231023-195931.ttl',
+      title: 'foo bar',
+      uploadDate: new Date('2023-10-23T19:59:31.424Z'),
+      readStatus: true,
       sender: 'PODMAN',
       senderWebId: 'http://localhost:3000/pod-test-name-here/profile/card#me',
       recipient: 'PODMAN'
@@ -38,6 +53,7 @@ const MockMessageContextValue = {
   setOutboxList: vi.fn(),
   loadMessages: false,
   setLoadMessages: vi.fn(),
+  setNumUnreadMessages: 1,
   updateMessageCountState: vi.fn()
 };
 
@@ -45,6 +61,9 @@ const MockSignupContexts = ({ session }) => (
   <BrowserRouter>
     <SessionContext.Provider value={session}>
       <MessageContext.Provider value={MockMessageContextValue}>
+        <IconButton aria-label="show new messages" dataTestid="EmailIcon">
+          <Badge badgeContent={MockMessageContextValue.setNumUnreadMessages} color="error" />
+        </IconButton>
         <Messages />
       </MessageContext.Provider>
     </SessionContext.Provider>
@@ -85,5 +104,52 @@ describe('Messages Page', () => {
     const { getByText, queryByText } = render(<MockSignupContexts session={sessionObj} />);
     expect(getByText('RE:test-inbox')).not.toBeNull();
     expect(queryByText('RE:test-outbox')).toBeNull();
+  });
+
+  it('should have an unread message', () => {
+    const sessionObj = {
+      login: vi.fn(),
+      fetch: vi.fn(),
+      podUrl: 'https://example.com',
+      session: {
+        info: {
+          webId: 'https://example.com/profile/',
+          isLoggedIn: true
+        }
+      }
+    };
+
+    render(<MockSignupContexts session={sessionObj} />);
+    const messageBadge = screen.getAllByText('1', { exact: true })[0];
+    const messageLabel = screen.getByLabelText('show new messages');
+    expect(messageBadge).not.toBeNull();
+    expect(messageLabel).not.toBeNull();
+  });
+
+  it('should update unread count after clicking on message', async () => {
+    const sessionObj = {
+      login: vi.fn(),
+      fetch: vi.fn(),
+      podUrl: 'https://example.com',
+      session: {
+        info: {
+          webId: 'https://example.com/profile/',
+          isLoggedIn: true
+        }
+      }
+    };
+
+    const user = userEvent.setup();
+
+    render(<MockSignupContexts session={sessionObj} />);
+    const messageBadge = screen.getAllByText('1', { exact: true })[0];
+    const messageLabel = screen.getByLabelText('show new messages');
+    expect(messageBadge).not.toBeNull();
+    expect(messageLabel).not.toBeNull();
+
+    const unreadMessage = screen.queryByText('RE:test-inbox').parentElement;
+    expect(unreadMessage).not.toBeNull();
+    await user.click(unreadMessage);
+    console.log(screen.debug());
   });
 });
