c4f199477058d68e30266d96c4bebc78ea188cbd
Zakeriya <muhumed@pdx.edu>
2023-11-07 17:56:30 -0800

    Camera button works for both desktop and mobile

--- src/components/Modals/UploadButtonGroup.jsx
+++ src/components/Modals/UploadButtonGroup.jsx
@@ -1,12 +1,14 @@
 // React Imports
-import React from 'react';
+import React, { useState } from 'react';
+
 // Material UI Imports
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import SearchIcon from '@mui/icons-material/Search';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
+import WebcamModal from './WebcamModal';
+// import useMediaQuery from '@mui/material/useMediaQuery';
+// import { useTheme } from '@mui/material/styles';
 
 /**
  * The UploadButtonGroup Component is a component that renders the upload document
@@ -19,22 +21,48 @@ import { useTheme } from '@mui/material/styles';
  * @param {Function} Props.setFile - The set function for handling files
  * @returns {React.JSX.Element} - The UploadButtonGroup Component
  */
+
+const dataURItoBlob = (dataURI) => {
+  // Convert base64/URLEncoded data component to raw binary data held in a string
+  let byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0) byteString = atob(dataURI.split(',')[1]);
+  else byteString = unescape(dataURI.split(',')[1]);
+
+  // Separate the mime component
+  const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  // Write the bytes of the string to a typed array
+  const ia = new Uint8Array(byteString.length);
+  for (let i = 0; i < byteString.length; i += 1) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], { type: mimeString });
+};
+
 const UploadButtonGroup = ({ file, setFile }) => {
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
-  const isCustomMediumScreen = useMediaQuery('(max-width: 768px)');
+  const [showWebcamModal, setShowWebcamModal] = useState(false);
+
+  const handleCapture = (imageSrc) => {
+    const now = new Date();
+    const imageFilename = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(
+      2,
+      '0'
+    )}-${String(now.getDate()).padStart(2, '0')}_${String(now.getHours()).padStart(
+      2,
+      '0'
+    )}-${String(now.getMinutes()).padStart(2, '0')}-${String(now.getSeconds()).padStart(
+      2,
+      '0'
+    )}.jpg`;
+
+    const imageBlob = dataURItoBlob(imageSrc);
+    const imageFile = new File([imageBlob], imageFilename, { type: 'image/jpeg' });
+    setFile(imageFile);
+  };
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        padding: '8px 8px 0',
-        flexDirection: isSmallScreen ? 'column' : 'row',
-        gap: isSmallScreen ? '10px' : '8px',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
+    <Box sx={{ display: 'flex', padding: '8px 8px 0', boxSizing: 'border-box' }}>
       <Button
         variant={file ? 'outlined' : 'contained'}
         component="label"
@@ -54,25 +82,25 @@ const UploadButtonGroup = ({ file, setFile }) => {
           accept=".pdf, .docx, .doc, .txt, .rtf, .gif, .png, .jpeg, .jpg, .webp"
         />
       </Button>
-      {isCustomMediumScreen && (
-        <Button
-          variant={file ? 'outlined' : 'contained'}
-          component="label"
-          color="primary"
-          id="upload-doctype"
-          name="uploadDoctype"
-          onChange={(e) => setFile(e.target.files[0])}
-          fullWidth
-          required
-          startIcon={<PhotoCameraIcon />}
-          sx={{ borderRadius: '20px' }}
-        >
-          Capture image
-          <input type="file" hidden accept="image/*" capture="environment" />
-        </Button>
-      )}
+
+      <Button
+        variant={file ? 'outlined' : 'contained'}
+        component="label"
+        color="primary"
+        onClick={() => setShowWebcamModal(true)}
+        fullWidth
+        startIcon={<PhotoCameraIcon />}
+        sx={{ borderRadius: '20px', marginLeft: '8px' }}
+      >
+        Use Webcam
+      </Button>
+
+      <WebcamModal
+        open={showWebcamModal}
+        onClose={() => setShowWebcamModal(false)}
+        onCapture={handleCapture}
+      />
     </Box>
   );
 };
-
 export default UploadButtonGroup;
---
+++ src/components/Modals/WebcamModal.jsx
@@ -0,0 +1,46 @@
+import React, { useRef } from 'react';
+import Webcam from 'react-webcam';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+
+const WebcamModal = ({ open, onClose, onCapture }) => {
+  const webcamRef = useRef(null);
+
+  const handleCapture = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    onCapture(imageSrc);
+    onClose();
+  };
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="webcam-modal-title"
+      aria-describedby="webcam-modal-description"
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh'
+        }}
+      >
+        <h2 id="webcam-modal-title">Webcam</h2>
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          videoConstraints={{ facingMode: 'user' }}
+        />
+        <Button variant="contained" color="primary" onClick={handleCapture}>
+          Take Photo
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default WebcamModal;
