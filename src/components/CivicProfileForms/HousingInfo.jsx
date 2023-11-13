0386dd6fd2e5cf3e545fb9ec62e53b34dad1fc31
Tim Standen <timmstanden@gmail.com>
2023-11-12 17:00:31 -0800

    Add npm start command and an address form

--- src/components/CivicProfileForms/HousingInfo.jsx
+++ src/components/CivicProfileForms/HousingInfo.jsx
@@ -1,8 +1,64 @@
 // React Imports
-import React from 'react';
+import React, { useState } from 'react';
 // MUI Imports
-import Typography from '@mui/material/Typography';
+import { TextField, Button } from '@mui/material';
 
-const HousingInfo = () => <Typography>Housing Info</Typography>;
+const HousingInfo = () => {
+  const [formData, setFormData] = useState({ street: '', city: '', state: '', zip: '' });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div
+        style={{
+          padding: '8px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '8px',
+          maxWidth: '360px'
+        }}
+      >
+        <TextField
+          id="street-input"
+          name="street"
+          label="Street:"
+          onChange={handleChange}
+          value={formData.street}
+        />
+        <TextField
+          id="city-input"
+          name="city"
+          label="City:"
+          onChange={handleChange}
+          value={formData.city}
+        />
+        <TextField
+          id="state-input"
+          name="state"
+          label="State:"
+          onChange={handleChange}
+          value={formData.state}
+        />
+        <TextField
+          id="zip-input"
+          name="zip"
+          label="ZIP Code:"
+          onChange={handleChange}
+          value={formData.zip}
+        />
+      </div>
+      <Button variant="outlined" type="submit" style={{ margin: '8px' }}>
+        Submit
+      </Button>
+    </form>
+  );
+};
 
 export default HousingInfo;
