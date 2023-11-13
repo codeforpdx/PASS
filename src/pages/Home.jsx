81e56fde3547661c1baa7c75b82f3fba2e837e1a
veganedge <scottxbrown@yahoo.com>
2023-11-13 11:40:06 -0800

    adding PASS info as h1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import Diversity1Icon from '@mui/icons-material/Diversity1';
 import SecurityIcon from '@mui/icons-material/Security';
 import Stack from '@mui/material/Stack';
 import SupportIcon from '@mui/icons-material/Support';
+import Typography from '@mui/material/Typography';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 // Components Import
@@ -23,6 +24,7 @@ import { HomeSection, KeyFeatures } from '@components/Home';
 const Home = () => {
   const theme = useTheme();
   const isReallySmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const iconSize = isReallySmallScreen ? 'medium' : 'large';
 
   return (
     <Container sx={{ width: '100vw' }}>
@@ -38,6 +40,20 @@ const Home = () => {
       >
         <section id="home">
           <Stack alignItems="center" justifyContent="center">
+            <Box my={8}>
+              <Box
+                component="img"
+                src="/assets/PASSLogolightmode.png"
+                alt=""
+                sx={{ width: isReallySmallScreen ? '80%' : '300px', color: 'primary.main' }}
+              />
+              <Typography variant="h1" color="primary" fontWeight="600">
+                PASS
+              </Typography>
+              <Typography variant="h2" component="p" color="primary.main" fontWeight="500">
+                Personal Access System for Services
+              </Typography>
+            </Box>
             <HomeSection
               isReallySmallScreen={isReallySmallScreen}
               componentImageSrc="/assets/web-security-green.png"
@@ -63,19 +79,19 @@ const Home = () => {
             />
             <KeyFeatures
               isReallySmallScreen={isReallySmallScreen}
-              icon={<SecurityIcon fontSize={isReallySmallScreen ? 'medium' : 'large'} />}
+              icon={<SecurityIcon fontSize={iconSize} />}
               title="Secure Storage"
               description="Store vital documents like IDs, Social Security information, birth certificates, medical records, and bank statements in a valid digital format."
             />
             <KeyFeatures
               isReallySmallScreen={isReallySmallScreen}
-              icon={<Diversity1Icon fontSize={isReallySmallScreen ? 'medium' : 'large'} />}
+              icon={<Diversity1Icon fontSize={iconSize} />}
               title="Nonprofit-Caseworker Integration"
               description="The platform facilitates smooth communication between nonprofit organizations, case workers, and the individuals they serve. It allows nonprofit organizations to maintain a contact list, and caseworkers are assigned contacts whose data they can access securely."
             />
             <KeyFeatures
               isReallySmallScreen={isReallySmallScreen}
-              icon={<SupportIcon fontSize={isReallySmallScreen ? 'medium' : 'large'} />}
+              icon={<SupportIcon fontSize={iconSize} />}
               title="Support Service"
               description="Verified documents can be used to facilitate access to service such as housing support and shelter accommodation. The platform simplifies the process of submitting necessary documents for such services."
             />
