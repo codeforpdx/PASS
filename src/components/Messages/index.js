71a1379cfd2a12ca6431f47b3d09708024c2ec4d
Ka Hung Lee <lee.kahung414@gmail.com>
2023-11-29 19:13:31 -0800

    Update unit tests

--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -3,6 +3,8 @@ import MessageFolder from './MessageFolder';
 import MessagesLayout from './MessagesLayout';
 import MessageButtonGroup from './MessageButtonGroup';
 import MESSAGE_PAGES_LIST from './MessagePagesList';
+import Inbox from './Inbox';
+import Outbox from './Outbox';
 
 /**
  * Components and functions related to Messages functionality within project PASS
@@ -10,4 +12,12 @@ import MESSAGE_PAGES_LIST from './MessagePagesList';
  * @namespace Messages
  */
 
-export { MessagePreview, MessageFolder, MessagesLayout, MessageButtonGroup, MESSAGE_PAGES_LIST };
+export {
+  MessagePreview,
+  MessageFolder,
+  MessagesLayout,
+  MessageButtonGroup,
+  MESSAGE_PAGES_LIST,
+  Inbox,
+  Outbox
+};
