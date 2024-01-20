aee0531ed49d516d3de44ef5e000ed6a40600146
DionSat <43707506+DionSat@users.noreply.github.com>
2024-01-20 12:21:14 -0800

    FormSection Independent

--- test/components/Form/FormSection.test.jsx
+++ test/components/Form/FormSection.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { expect, it } from 'vitest';
+import { cleanup, render } from '@testing-library/react';
+import { describe, afterEach, expect, it } from 'vitest';
 import { FormSection } from '@components/Form';
 import createMatchMedia from '../../helpers/createMatchMedia';
 
@@ -12,18 +12,25 @@ const MockFormSection = () => (
   </FormSection>
 );
 
-it('renders 20px padding by default', () => {
-  const component = render(<MockFormSection />);
-  const adjustableBox = getComputedStyle(component.container.firstChild);
+describe('Form Rendering', () => {
+  afterEach(() => {
+    cleanup();
+    window.matchMedia = null;
+  });
 
-  expect(adjustableBox.padding).toBe('20px');
-});
+  it('renders 20px padding by default', () => {
+    const component = render(<MockFormSection />);
+    const adjustableBox = getComputedStyle(component.container.firstChild);
 
-it("renders 10px padding after MUI breakpoint 'sm' is triggered", () => {
-  window.matchMedia = createMatchMedia(599);
+    expect(adjustableBox.padding).toBe('20px');
+  });
 
-  const component = render(<MockFormSection />);
-  const adjustableBox = getComputedStyle(component.container.firstChild);
+  it("renders 10px padding after MUI breakpoint 'sm' is triggered", () => {
+    window.matchMedia = createMatchMedia(599);
 
-  expect(adjustableBox.padding).toBe('10px');
+    const component = render(<MockFormSection />);
+    const adjustableBox = getComputedStyle(component.container.firstChild);
+
+    expect(adjustableBox.padding).toBe('10px');
+  });
 });
