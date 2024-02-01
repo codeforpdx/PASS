eef84200c64c56aa44ae61079f4f62f8601af486
Tim Standen <37914436+timbot1789@users.noreply.github.com>
2024-02-01 09:33:27 -0800

    Simplify resize tests (#574)
    * Simplify PASS dynamic resizing

--- src/components/Form/FormSection.jsx
+++ src/components/Form/FormSection.jsx
@@ -3,8 +3,6 @@ import React from 'react';
 // Material UI Imports
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
 
 /**
  * FormSection Component - Component that wraps section with title and MUI Box
@@ -19,29 +17,24 @@ import { useTheme } from '@mui/material/styles';
  * @param {React.ReactElement} Props.children - JSX Element of the wrapped form
  * @returns {React.JSX.Element} - The FormSection Component
  */
-const FormSection = ({ title, headingId, children }) => {
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
-
-  return (
-    <Box
-      sx={{
-        marginTop: 1,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        mx: '3px',
-        padding: isSmallScreen ? '10px' : '20px',
-        minWidth: '50%'
-      }}
-    >
-      <Typography variant="h2" align="center" mb={2} sx={{ fontSize: '20px' }} id={headingId}>
-        {title}
-      </Typography>
-      {children}
-    </Box>
-  );
-};
+const FormSection = ({ title, headingId, children }) => (
+  <Box
+    sx={{
+      marginTop: 1,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      mx: '3px',
+      padding: '16px',
+      minWidth: '50%'
+    }}
+  >
+    <Typography variant="h2" align="center" mb={2} sx={{ fontSize: '20px' }} id={headingId}>
+      {title}
+    </Typography>
+    {children}
+  </Box>
+);
 
 export default FormSection;
