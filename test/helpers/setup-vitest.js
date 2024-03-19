7580159557d09ad2016268cc97fe5b9b20638083
DionSat <43707506+DionSat@users.noreply.github.com>
2024-03-19 16:50:38 -0700

    Setup ENV variables in setupFile

--- test/helpers/setup-vitest.js
+++ test/helpers/setup-vitest.js
@@ -2,8 +2,8 @@
 import { afterEach } from 'vitest';
 import createMatchMedia from './createMatchMedia';
 
-// process.env.VITE_SOLID_IDENTITY_PROVIDER = 'https://solidcommunity.net',
-// process.env.VITE_SUGGESTED_OIDC_OPTIONS = 'http://testurl_1.com/, http://testurl_2.com/, http://testurl_3.com/'
+process.env.VITE_SOLID_IDENTITY_PROVIDER = 'https://solidcommunity.net',
+process.env.VITE_SUGGESTED_OIDC_OPTIONS = 'http://testurl_1.com/, http://testurl_2.com/, http://testurl_3.com/'
 
 afterEach(() => {
   window.matchMedia = createMatchMedia(1200);
