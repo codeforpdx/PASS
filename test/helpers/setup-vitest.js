9df38585a40071411d64ad326170edd645a61c25
DionSat <43707506+DionSat@users.noreply.github.com>
2024-03-23 10:46:08 -0700

    Ran Prettier for formatting

--- test/helpers/setup-vitest.js
+++ test/helpers/setup-vitest.js
@@ -5,7 +5,8 @@ import createMatchMedia from './createMatchMedia';
 process.env.VITE_SOLID_IDENTITY_PROVIDER = 'https://solidcommunity.net';
 process.env.VITE_SUGGESTED_OIDC_OPTIONS =
   'http://testurl_1.com/, http://testurl_2.com/, http://testurl_3.com/';
-process.env.VITE_OIDC_WEBIDS = '{"http://testurl_1.com/": "http://testurl_1.com/user/profile/card#me", "http://testurl_2.com/": "http://testurl_2.com/user/profile/card#me", "http://testurl_3.com/": "http://testurl_3.com/user/profile/card#me"}'
+process.env.VITE_OIDC_WEBIDS =
+  '{"http://testurl_1.com/": "http://testurl_1.com/user/profile/card#me", "http://testurl_2.com/": "http://testurl_2.com/user/profile/card#me", "http://testurl_3.com/": "http://testurl_3.com/user/profile/card#me"}';
 
 afterEach(() => {
   window.matchMedia = createMatchMedia(1200);
