2de98632ba6e0449d34d32997edb31488b2569a6
russfraze <russell.fraze@gmail.com>
2024-04-22 14:05:29 -0700

    import vi

--- test/components/Signup/ExistingPodForm.test.jsx
+++ test/components/Signup/ExistingPodForm.test.jsx
@@ -7,44 +7,38 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { expect, it, describe, vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
 
-
 // Component being tested
 import { ExistingPodForm } from '@components/Signup';
 import { login } from '@inrupt/solid-client-authn-browser';
 
 vi.mock('@inrupt/solid-client-authn-browser');
 
-
 describe('ExistingPodForm', () => {
-    it('renders', () => {
-      render(
-        <Router>
-          <ExistingPodForm />
-        </Router>
-      );
-  
-      const textField = screen.getByRole('textbox', { name: 'Pod Provider' });
-  
-      expect(textField).not.toBeNull();
-    });
+  it('renders', () => {
+    render(
+      <Router>
+        <ExistingPodForm />
+      </Router>
+    );
 
-    it('calls the login function when the form is submitted', async() => {
+    const textField = screen.getByRole('textbox', { name: 'Pod Provider' });
 
-
-        render(
-          <Router>
-            <ExistingPodForm />
-          </Router>
-        );
-        
-        let subButton = screen.getByRole('button', { name: "Login to Pod Provider" });
-        expect(subButton).not.toBeNull();
-    
-        const user = userEvent.setup();
-        await user.click(subButton);
-
-        expect(login).toHaveBeenCalled();
-      });
+    expect(textField).not.toBeNull();
   });
 
- 
+  it('calls the login function when the form is submitted', async () => {
+    render(
+      <Router>
+        <ExistingPodForm />
+      </Router>
+    );
+
+    const subButton = screen.getByRole('button', { name: 'Login to Pod Provider' });
+    expect(subButton).not.toBeNull();
+
+    const user = userEvent.setup();
+    await user.click(subButton);
+
+    expect(login).toHaveBeenCalled();
+  });
+});
