cdae42e89ae69f93bd78d3a48e65ad5df1d82697
Andy <awilliamscomposing@gmail.com>
2024-04-29 23:58:06 -0700

    Adding JSDocs

--- src/utils/copy/saveToClipboard.js
+++ src/utils/copy/saveToClipboard.js
@@ -1,3 +1,13 @@
+/**
+ * Function that saves the provided text to the clipboard and sends the user a notification.
+ *
+ * @memberof utils
+ * @function saveToClipboard
+ * @param {string} text - Text to be saved to the clipboard
+ * @param {string} message - Message to be displayed in the notification
+ * @param {Function} addNotification - Function to display the notification
+ * @returns {void} - This function does not have a return
+ */
 const saveToClipboard = (text, message, addNotification) => {
   navigator.clipboard.writeText(text);
   addNotification('success', message);
