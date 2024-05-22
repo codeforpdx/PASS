84a47c2ceadd5a48e0faedf61c2147e169b3aad5
Faddah Wolf <my_biz@me.com>
2024-05-21 19:41:23 -0700

    Added FormSection & @mui FormControl as used in BasicInfo.jsx per Andy C.W.'s comment in PR #633 (https://github.com/codeforpdx/PASS/pull/633#discussion_r1605901809).

--- src/components/CivicProfileForms/HousingInfo.jsx
+++ src/components/CivicProfileForms/HousingInfo.jsx
@@ -1,10 +1,12 @@
 // React Imports
 import React, { useState, useEffect } from 'react';
 // MUI Imports
+import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 // Custom Hooks Imports
 import { useCivicProfile } from '@hooks';
+import { FormSection } from '../Form';
 
 /**
  * HousingInfo - A form to fill out with housing security info
@@ -50,107 +52,108 @@ const HousingInfo = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '8px',
-        justifyContent: 'space-between'
-        // maxWidth: '720px',
-      }}
-    >
-      <div
+    <FormSection title="Housing Information">
+      <FormControl
+        onSubmit={handleSubmit}
         style={{
-          display: 'flex',
-          gap: '8px',
-          minWidth: '846px',
-          padding: '8px'
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '8px',
-            padding: '8px',
-            width: '40%'
-          }}
-        >
-          <TextField
-            id="street-input"
-            name="lastPermanentStreet"
-            label="Street:"
-            onChange={handleChange}
-            value={formData.lastPermanentStreet}
-          />
-          <TextField
-            id="city-input"
-            name="lastPermanentCity"
-            label="City:"
-            onChange={handleChange}
-            value={formData.lastPermanentCity}
-          />
-          <TextField
-            id="state-input"
-            name="lastPermanentState"
-            label="State:"
-            onChange={handleChange}
-            value={formData.lastPermanentState}
-          />
-          <TextField
-            id="zip-input"
-            name="lastPermanentZIP"
-            label="ZIP Code:"
-            onChange={handleChange}
-            value={formData.lastPermanentZIP}
-          />
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '8px',
-            padding: '8px',
-            width: '40%'
-          }}
-        >
-          <TextField
-            id="months-homeless-input"
-            name="monthsHomeless"
-            label="Months Houseless Past 3 Years:"
-            onChange={handleChange}
-            value={formData.monthsHomeless}
-          />
-          <TextField
-            id="times-homeless-input"
-            name="timesHomeless"
-            label="Number of Times Houseless Past 3 Years:"
-            onChange={handleChange}
-            value={formData.timesHomeless}
-          />
-          <TextField
-            id="time-to-housing-loss-input"
-            name="timeToHousingLoss"
-            label="Time Until Loss of Housing:"
-            onChange={handleChange}
-            value={formData.timeToHousingLoss}
-          />
-        </div>
-      </div>
-      <div
-        style={{
-          alignSelf: 'flex-start',
           display: 'flex',
           flexDirection: 'column',
-          padding: '8px'
+          gap: '8px',
+          justifyContent: 'space-between'
         }}
       >
-        <Button variant="outlined" disabled={!isSuccess} type="submit" style={{ margin: '8px' }}>
-          Submit
-        </Button>
-      </div>
-    </form>
+        <div
+          style={{
+            display: 'flex',
+            gap: '8px',
+            minWidth: '846px',
+            padding: '8px'
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '8px',
+              padding: '8px',
+              width: '40%'
+            }}
+          >
+            <TextField
+              id="street-input"
+              name="lastPermanentStreet"
+              label="Street:"
+              onChange={handleChange}
+              value={formData.lastPermanentStreet}
+            />
+            <TextField
+              id="city-input"
+              name="lastPermanentCity"
+              label="City:"
+              onChange={handleChange}
+              value={formData.lastPermanentCity}
+            />
+            <TextField
+              id="state-input"
+              name="lastPermanentState"
+              label="State:"
+              onChange={handleChange}
+              value={formData.lastPermanentState}
+            />
+            <TextField
+              id="zip-input"
+              name="lastPermanentZIP"
+              label="ZIP Code:"
+              onChange={handleChange}
+              value={formData.lastPermanentZIP}
+            />
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '8px',
+              padding: '8px',
+              width: '40%'
+            }}
+          >
+            <TextField
+              id="months-homeless-input"
+              name="monthsHomeless"
+              label="Months Houseless Past 3 Years:"
+              onChange={handleChange}
+              value={formData.monthsHomeless}
+            />
+            <TextField
+              id="times-homeless-input"
+              name="timesHomeless"
+              label="Number of Times Houseless Past 3 Years:"
+              onChange={handleChange}
+              value={formData.timesHomeless}
+            />
+            <TextField
+              id="time-to-housing-loss-input"
+              name="timeToHousingLoss"
+              label="Time Until Loss of Housing:"
+              onChange={handleChange}
+              value={formData.timeToHousingLoss}
+            />
+          </div>
+        </div>
+        <div
+          style={{
+            alignSelf: 'flex-start',
+            display: 'flex',
+            flexDirection: 'column',
+            padding: '8px'
+          }}
+        >
+          <Button variant="outlined" disabled={!isSuccess} type="submit" style={{ margin: '8px' }}>
+            Submit
+          </Button>
+        </div>
+      </FormControl>
+    </FormSection>
   );
 };
 
