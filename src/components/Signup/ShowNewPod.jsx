0584f37d0702f152b3c110ebf4601ca12e619985
Andrew Sterner <andrewjohnsterner@gmail.com>
2024-05-28 18:18:03 -0700

    645: Fix links in pod registration card
    Link rendering was broken in the page that loads after the successful
    creation of a pod. This commit updates the page to render the links
    correctly.

--- src/components/Signup/ShowNewPod.jsx
+++ src/components/Signup/ShowNewPod.jsx
@@ -29,17 +29,29 @@ const ShowNewPod = ({ oidcIssuer, podUrl, webId }) => {
         <h1>You have logged in with your existing pod.</h1>
       )}
       <Typography>
-        {podUrl &&
-          `You can access your pod through your Pod Provider: <a href=${oidcIssuer}>${oidcIssuer}</a>
-        <br />
-        We set up this pod to operate with PASS: <a href=${podUrl}>${podUrl}</a>
-        <br />`}
-        Your Pod Provider has created this webId for you: <a href={webId}>{webId}</a>
-        <br />
+        {podUrl && (
+          <>
+            {'You can access your pod through your Pod Provider: '}
+            <Link to={oidcIssuer}>{oidcIssuer}</Link>
+            <br />
+            {'We set up this pod to operate with PASS: '}
+            <Link to={podUrl}>{podUrl}</Link>
+            <br />
+          </>
+        )}
+        {webId && (
+          <>
+            {'Your Pod Provider has created this webId for you: '}
+            <Link href={webId}>{webId}</Link>
+            <br />
+          </>
+        )}
         {isSuccess && data.length > 0
-          ? `You have registered with ${data[0].person}`
-          : 'You have not registered with a case manager'}
+          ? `You have registered with ${data[0].person}.`
+          : 'You have not registered with a case manager.'}
       </Typography>
+
+      <br />
       <Typography>Click here to return to the home page and log into PASS:</Typography>
       <Button>
         <Link to={window.location.origin}>Homepage</Link>
