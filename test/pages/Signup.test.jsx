cca1e1dbbc164c3953e34e2f17d07558ddd133b4
Jae <giffordjacob0@gmail.com>
2024-06-01 10:06:09 -0700

    added test for email existing

--- test/pages/Signup.test.jsx
+++ test/pages/Signup.test.jsx
@@ -7,7 +7,16 @@ import { Signup } from '@pages';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionContext } from '@contexts';
 import { initializePod, registerPod } from '@components/Signup';
+import { useNotification } from '@hooks';
 
+vi.mock('@hooks', async () => {
+  const actual = await vi.importActual('@hooks');
+
+  return {
+    ...actual,
+    useNotification: vi.fn()
+  };
+});
 vi.mock('react-router-dom', async () => {
   const originalModule = await vi.importActual('react-router-dom');
 
@@ -23,7 +32,13 @@ vi.mock('@components/Signup', async () => {
   return {
     ...orig,
     initializePod: vi.fn(),
-    registerPod: vi.fn(() => Promise.resolve({ webId: '', podUrl: '', fetch: vi.fn() }))
+    registerPod: vi.fn((email) => {
+      if (email && /emailExists/.test(email)) {
+        // pretending this email is already in solid
+        return Promise.reject(new Error());
+      }
+      return Promise.resolve({ webId: '', podUrl: '', fetch: vi.fn() });
+    })
   };
 });
 
@@ -58,6 +73,7 @@ describe('Signup Page', () => {
         }
       }
     };
+    useNotification.mockReturnValue({ addNotification: vi.fn() });
     const { getByRole } = render(<MockSignupContexts session={sessionObj} />);
     expect(getByRole('heading', { name: 'Register For PASS' })).not.toBeNull();
   });
@@ -89,8 +105,40 @@ describe('Signup Page', () => {
     await user.type(confirmPasswordField, confirmPassword);
     await user.click(getAllByRole('button')[2]);
 
-    expect(registerPod).toBeCalledTimes(1);
+    expect(registerPod).toHaveBeenCalledOnce();
     expect(initializePod).toBeCalledTimes(1);
+  });
+  it('it fails if email is already registered', async () => {
+    const session = {
+      login: vi.fn(),
+      fetch: vi.fn(),
+      podUrl: 'https://example.com',
+      session: {
+        info: {
+          webId: 'https://example.com/profile/',
+          isLoggedIn: false
+        }
+      }
+    };
+    useNotification.mockReturnValue({ addNotification: vi.fn() });
+
+    const user = userEvent.setup();
+    const { getByRole, getAllByRole, getByLabelText } = render(
+      <MockSignupContexts session={session} />
+    );
+    global.fetch = vi.fn(() => Promise.resolve({ json: vi.fn() }));
+    const email = 'emailExists@example.com';
+    const password = 'password';
+    const confirmPassword = 'password';
+    const emailField = getByRole('textbox', { name: 'Email' });
+    const passwordField = getByLabelText('Password');
+    const confirmPasswordField = getByLabelText('Confirm Password');
+
+    await user.type(emailField, email);
+    await user.type(passwordField, password);
+    await user.type(confirmPasswordField, confirmPassword);
+    await user.click(getAllByRole('button')[2]);
+    expect(registerPod(emailField.value)).rejects.toThrow();
   });
   it('shows pod creation message when logged in', () => {
     const session = {
