073b5fc8c7d4806a292206a879c4fdd19df836f0
Faddah Wolf <my_biz@me.com>
2024-06-05 18:25:28 -0700

    With assistance @leekahung, corrected how `<Select>` fields are tested with `expect` looking at `firstChild.textContent` for each to be `'Data not collected'`.

--- test/components/CivicProfileForms/HousingInfo.test.jsx
+++ test/components/CivicProfileForms/HousingInfo.test.jsx
@@ -116,4 +116,58 @@ describe('Housing info form', () => {
 
     expect(mockAdd).not.toBeCalled();
   });
+
+  it('clears all input fields when you click the clear button', async () => {
+    const user = userEvent.setup();
+    const mockClear = vi.fn();
+    const address = {
+      lastPermanentStreet: '20th ave',
+      lastPermanentCity: 'Portland',
+      lastPermanentState: 'Oregon',
+      lastPermanentZIP: '97205',
+      monthsHomeless: '1 Month',
+      timesHomeless: 'One Time',
+      timeToHousingLoss: '7-13 Days'
+    };
+    useCivicProfile.mockReturnValue({
+      add: mockClear,
+      isSuccess: true,
+      storedDataset: {},
+      refetch: vi.fn()
+    });
+
+    useNotification.mockReturnValue({ addNotification: vi.fn() });
+    const { getByRole } = render(<HousingInfo />);
+    const cityField = getByRole('textbox', { name: 'City:' });
+    const streetField = getByRole('textbox', { name: 'Street:' });
+    const stateField = getByRole('textbox', { name: 'State:' });
+    const zipField = getByRole('textbox', { name: 'ZIP Code:' });
+    const monthsHomelessField = getByRole('combobox', { name: 'Months Houseless Past 3 Years:' });
+    const timesHomelessField = getByRole('combobox', {
+      name: 'Number of Times Houseless Past 3 Years:'
+    });
+    const timeToHousingLossField = getByRole('combobox', { name: 'Time Until Loss of Housing:' });
+    const clearButton = getByRole('button', { name: 'Clear button' });
+
+    await user.type(streetField, address.lastPermanentStreet);
+    await user.type(cityField, address.lastPermanentCity);
+    await user.type(stateField, address.lastPermanentState);
+    await user.type(zipField, address.lastPermanentZIP);
+    await user.click(monthsHomelessField);
+    await user.click(screen.getByRole('option', { name: address.monthsHomeless }));
+    await user.click(timesHomelessField);
+    await user.click(screen.getByRole('option', { name: address.timesHomeless }));
+    await user.click(timeToHousingLossField);
+    await user.click(screen.getByRole('option', { name: address.timeToHousingLoss }));
+
+    await user.click(clearButton);
+
+    expect(streetField.value).toBe('');
+    expect(cityField.value).toBe('');
+    expect(stateField.value).toBe('');
+    expect(zipField.value).toBe('');
+    expect(monthsHomelessField.firstChild.textContent).toBe('Data not collected');
+    expect(timesHomelessField.firstChild.textContent).toBe('Data not collected');
+    expect(timeToHousingLossField.firstChild.textContent).toBe('Data not collected');
+  });
 });
