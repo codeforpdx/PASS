01e75110a36770bac8fd0d34c7d662e0aeb58379
Andy <awilliamscomposing@gmail.com>
2024-08-06 12:17:21 -0700

    Adding tests for desktop contacts table

--- test/components/Contacts/ContactListTableDesktop.test.jsx
+++ test/components/Contacts/ContactListTableDesktop.test.jsx
@@ -1,47 +1,82 @@
-// import React from 'react';
-// import { BrowserRouter } from 'react-router-dom';
-// import { render } from '@testing-library/react';
-// import { expect, it } from 'vitest';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import ContactListTableDesktop from '../../../src/components/Contacts/ContactListTableDesktop';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { cleanup, render, screen, within } from '@testing-library/react';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import { SessionContext } from '@contexts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ContactListTableDesktop } from '@components/Contacts';
 
-// // Clear created DOM after each test
-// afterEach(() => {
-//   cleanup();
-// });
+// Clear created DOM after each test
+afterEach(() => {
+  cleanup();
+});
 
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
-// const MockTableComponent = ({
-// deleteContact,
-// editContact,
-// handleSendMessage,
-// 'data-testid': dataTestId }) => {
-//   window.matchMedia = createMatchMedia(isSmallScreen ? 500 : 1200);
+const MockTableComponent = ({
+  contacts,
+  deleteContact = vi.fn(),
+  editContact = vi.fn(),
+  handleSendMessage = vi.fn(),
+  'data-testid': dataTestId = 'contacts-table',
+  sessionObj
+}) => (
+  <QueryClientProvider client={queryClient}>
+    <SessionContext.Provider value={sessionObj}>
+      <BrowserRouter>
+        <ContactListTableDesktop
+          contacts={contacts}
+          deleteContact={deleteContact}
+          editContact={editContact}
+          handleSendMessage={handleSendMessage}
+          data-testid={dataTestId}
+        />
+      </BrowserRouter>
+    </SessionContext.Provider>
+  </QueryClientProvider>
+);
 
-//   return render(
-//     <QueryClientProvider client={queryClient}>
-//       <SessionContext.Provider>
-//         <BrowserRouter>
-//           <ContactListTableDesktop
-//           />
-//         </BrowserRouter>
-//       </SessionContext.Provider>
-//     </QueryClientProvider>
-//   );
-// };
+const contacts = [
+  {
+    familyName: 'Abby',
+    givenName: 'Aaron',
+    person: 'Aaron Abby',
+    webId: 'https://example.com/Abby'
+  },
+  {
+    familyName: 'Builder',
+    givenName: 'Bob',
+    person: 'Bob Builder',
+    webId: 'https://example.com/Builder'
+  }
+];
 
-// const contacts = [
-//   {
-//     familyName: 'Abby',
-//     givenName: 'Aaron',
-//     person: 'Aaron Abby',
-//     webId: 'https://example.com/Abby'
-//   },
-//   {
-//     familyName: 'Builder',
-//     givenName: 'Bob',
-//     person: 'Bob Builder',
-//     webId: 'https://example.com/Builder'
-//   }
-// ];
+describe('contacts list table desktop tests', () => {
+  const sessionObj = {
+    login: vi.fn(),
+    fetch: vi.fn(),
+    podUrl: 'https://example.com',
+    session: {
+      fetch: vi.fn(),
+      info: {
+        webId: 'https://example.com/profile/',
+        isLoggedIn: true
+      }
+    }
+  };
+
+  it('renders', () => {
+    render(<MockTableComponent contacts={contacts} session={sessionObj} />);
+  });
+
+  it('renders with all clients from client context', () => {
+    render(<MockTableComponent contacts={contacts} />);
+
+    const dataGrid = screen.getByTestId('contacts-table');
+
+    contacts.forEach((contact) => {
+      expect(within(dataGrid).getByText(contact.givenName)).not.toBeNull();
+      expect(within(dataGrid).getByText(contact.familyName)).not.toBeNull();
+    });
+  });
+});
--- test/components/Contacts/ContactListTableMobile.test.jsx
+++ test/components/Contacts/ContactListTableMobile.test.jsx
@@ -1,47 +1,56 @@
-// import React from 'react';
-// import { BrowserRouter } from 'react-router-dom';
-// import { render } from '@testing-library/react';
-// import { expect, it } from 'vitest';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import ContactListTableMobile from '../../../src/components/Contacts/ContactListTableMobile';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { cleanup, render, screen, within } from '@testing-library/react';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import { SessionContext } from '@contexts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ContactListTableMobile } from '@components/Contacts';
 
-// // Clear created DOM after each test
-// afterEach(() => {
-//   cleanup();
-// });
+// Clear created DOM after each test
+afterEach(() => {
+  cleanup();
+});
 
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
-// const MockTableComponent = ({ 
-// deleteContact,
-// editContact,
-// handleSendMessage,
-// 'data-testid': dataTestId }) => {
-//   window.matchMedia = createMatchMedia(isSmallScreen ? 500 : 1200);
+const MockTableComponent = ({
+  contacts,
+  deleteContact = vi.fn(),
+  editContact = vi.fn(),
+  handleSendMessage = vi.fn(),
+  'data-testid': dataTestId = 'contacts-table',
+  sessionObj
+}) => (
+  <QueryClientProvider client={queryClient}>
+    <SessionContext.Provider value={sessionObj}>
+      <BrowserRouter>
+        <ContactListTableMobile
+          contacts={contacts}
+          deleteContact={deleteContact}
+          editContact={editContact}
+          handleSendMessage={handleSendMessage}
+          data-testid={dataTestId}
+        />
+      </BrowserRouter>
+    </SessionContext.Provider>
+  </QueryClientProvider>
+);
 
-//   return render(
-//     <QueryClientProvider client={queryClient}>
-//       <SessionContext.Provider>
-//         <BrowserRouter>
-//           <ContactListTableMobile
-//           />
-//         </BrowserRouter>
-//       </SessionContext.Provider>
-//     </QueryClientProvider>
-//   );
-// };
+const contacts = [
+  {
+    familyName: 'Abby',
+    givenName: 'Aaron',
+    person: 'Aaron Abby',
+    webId: 'https://example.com/Abby'
+  },
+  {
+    familyName: 'Builder',
+    givenName: 'Bob',
+    person: 'Bob Builder',
+    webId: 'https://example.com/Builder'
+  }
+];
 
-// const contacts = [
-//   {
-//     familyName: 'Abby',
-//     givenName: 'Aaron',
-//     person: 'Aaron Abby',
-//     webId: 'https://example.com/Abby'
-//   },
-//   {
-//     familyName: 'Builder',
-//     givenName: 'Bob',
-//     person: 'Bob Builder',
-//     webId: 'https://example.com/Builder'
-//   }
-// ];
+describe('contacts list table mobile tests', () => {
+  it.todo('renders all clients from client context');
+});
