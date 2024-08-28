01f2c44a228bf09d846b1c38ffde26b91c39ecb8
Andrew Sterner <andrewjohnsterner@gmail.com>
2024-08-28 10:43:42 -0700

    fix vite config formatting

--- vite.config.js
+++ vite.config.js
@@ -4,32 +4,32 @@ import visualizer from 'rollup-plugin-visualizer';
 import { resolve } from "path";
 
 export default defineConfig({
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
+    build: {
+      manifest: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+        }
       }
+    },
+    plugins: [
+      react(),
+      visualizer({
+        filename: 'bundleStats.html'
+      }),
+    ],
+    resolve:{
+      alias:[
+        {find: "@hooks", replacement:  resolve(__dirname, "src/hooks")},
+        {find: "@contexts", replacement: resolve(__dirname, "src/contexts")},
+        {find: "@pages", replacement: resolve(__dirname, "src/pages")},
+        {find: "@utils", replacement: resolve(__dirname, "src/utils")},
+        {find: "@components", replacement: resolve(__dirname,"src/components")},
+        {find: "@constants", replacement: resolve(__dirname,"src/constants")},
+      ]
+    },
+    test: {
+      environment: 'jsdom'
     }
-  },
-  plugins: [
-    react(),
-    visualizer({
-      filename: 'bundleStats.html'
-    }),
-  ],
-  resolve: {
-    alias: [
-      { find: "@hooks", replacement: resolve(__dirname, "src/hooks") },
-      { find: "@contexts", replacement: resolve(__dirname, "src/contexts") },
-      { find: "@pages", replacement: resolve(__dirname, "src/pages") },
-      { find: "@utils", replacement: resolve(__dirname, "src/utils") },
-      { find: "@components", replacement: resolve(__dirname, "src/components") },
-      { find: "@constants", replacement: resolve(__dirname, "src/constants") },
-    ]
-  },
-  test: {
-    environment: 'jsdom'
-  }
 });
 
