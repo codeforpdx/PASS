570c59559b02b2c9e011ec0f7482dc2aae8888fe
Andy <awilliamscomposing@gmail.com>
2024-10-03 12:31:13 -0700

    Migrating relevant tests to new file

---
+++ test/pages/Documents.test.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { act } from 'react-dom/test-utils';
+import { render, screen, cleanup, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { describe, expect, it, beforeEach, afterEach } from 'vitest';
+import { Documents } from '@pages';
+import MockSignedInUserContext from '../mocks/contexts/MockSignedInUserContext';
+
+const queryClient = new QueryClient();
+
+const renderDocuments = () =>
+  render(
+    <QueryClientProvider client={queryClient}>
+      <MockSignedInUserContext>
+        <BrowserRouter>
+          <Documents />
+        </BrowserRouter>
+      </MockSignedInUserContext>
+    </QueryClientProvider>
+  );
+
+describe('Documents Page', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  beforeEach(() => {
+    act(() => {
+      renderDocuments();
+    });
+  });
+
+  it('renders', async () => {
+    const heading = await screen.findByRole('heading', { name: 'My Documents' }, { timeout: 2000 });
+    expect(heading).toHaveAccessibleName('My Documents');
+  });
+
+  it('can show and hide the share documents modal', async () => {
+    const user = userEvent.setup();
+
+    const shareDocumentsButton = await screen.findByRole(
+      'button',
+      { name: 'Share Documents' },
+      { timeout: 2000 }
+    );
+    expect(shareDocumentsButton).toHaveAccessibleName('Share Documents');
+
+    // Open share document modal
+    user.click(shareDocumentsButton);
+    const shareHeading = await screen.findByRole('heading', { name: 'Share All Documents' });
+    expect(shareHeading).toBeVisible();
+
+    // Close share document modal
+    user.keyboard('{escape}');
+    await waitForElementToBeRemoved(shareHeading, { timeout: 5000 });
+  });
+
+  it('can show and hide the add document modal', async () => {
+    const user = userEvent.setup();
+
+    const addDocumentButton = await screen.findByRole(
+      'button',
+      { name: 'Add Document' },
+      { timeout: 2000 }
+    );
+    expect(addDocumentButton).toHaveAccessibleName('Add Document');
+
+    // Open add document modal
+    user.click(addDocumentButton);
+    const addHeading = await screen.findByRole('heading', { name: 'Upload Document' });
+
+    // close add document modal
+    user.keyboard('{escape}');
+    await waitForElementToBeRemoved(addHeading, { timeout: 5000 });
+  });
+});
